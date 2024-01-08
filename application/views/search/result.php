4c380810f3078255881202d604cc3d94a61f9742
phl0 <github@florian-wolters.de>
2024-01-08 16:20:36 +0100

    Add missing space

--- application/views/search/result.php
+++ application/views/search/result.php
@@ -55,7 +55,7 @@
 			} elseif ($lotw_lastupload > 7) {
 				$lotw_hint = ' lotw_info_yellow';
 			}
-			echo '<a id="lotw_badge" href="https://lotw.arrl.org/lotwuser/act?act='.$callsign['callsign'].'" target="_blank"><small id="lotw_info" class="badge text-bg-success'.$lotw_hint.'" data-bs-toggle="tooltip" title="LoTW User">Yes</small></a> <a id="lotw_badge" href="https://lotw.arrl.org/lotwuser/act?act='.$callsign['callsign'].'" target="_blank">last upload</a> '.$lotw_lastupload.' days ago';
+			echo '<a id="lotw_badge" href="https://lotw.arrl.org/lotwuser/act?act='.$callsign['callsign'].'" target="_blank"><small id="lotw_info" class="badge text-bg-success'.$lotw_hint.'" data-bs-toggle="tooltip" title="LoTW User">Yes</small></a> <a id="lotw_badge" href="https://lotw.arrl.org/lotwuser/act?act='.$callsign['callsign'].'" target="_blank"> last upload</a> '.$lotw_lastupload.' days ago';
 		} else {
 			echo "<span data-bs-toggle=\"tooltip\" title=\"No LoTW User\" class=\"badge text-bg-danger\" style=\"padding-left: 0.2em; padding-right: 0.2em;\">No</span>";
 		}
