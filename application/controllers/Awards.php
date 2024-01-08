f9dbaad0ea92dcad8d6843d7a94a4687e70e4ba2
Andreas <6977712+AndreasK79@users.noreply.github.com>
2024-01-08 10:17:53 +0100

    [Awards] Added RAC

--- application/controllers/Awards.php
+++ application/controllers/Awards.php
@@ -505,6 +505,65 @@ class Awards extends CI_Controller {
         $this->load->view('interface_assets/footer', $footerData);
     }
 
+	public function rac() {
+		$footerData = [];
+		$footerData['scripts'] = [
+			'assets/js/sections/racmap_geojson.js?' . filemtime(realpath(__DIR__ . "/../../assets/js/sections/racmap_geojson.js")),
+			'assets/js/sections/racmap.js?' . filemtime(realpath(__DIR__ . "/../../assets/js/sections/racmap.js"))
+		];
+
+        $this->load->model('rac');
+		$this->load->model('modes');
+        $this->load->model('bands');
+
+        $data['worked_bands'] = $this->bands->get_worked_bands('rac');
+		$data['modes'] = $this->modes->active(); // Used in the view for mode select
+
+        if ($this->input->post('band') != NULL) {   // Band is not set when page first loads.
+            if ($this->input->post('band') == 'All') {         // Did the user specify a band? If not, use all bands
+                $bands = $data['worked_bands'];
+            }
+            else {
+                $bands[] = $this->security->xss_clean($this->input->post('band'));
+            }
+        }
+        else {
+            $bands = $data['worked_bands'];
+        }
+
+        $data['bands'] = $bands; // Used for displaying selected band(s) in the table in the view
+
+        if($this->input->method() === 'post') {
+            $postdata['qsl'] = $this->security->xss_clean($this->input->post('qsl'));
+            $postdata['lotw'] = $this->security->xss_clean($this->input->post('lotw'));
+            $postdata['eqsl'] = $this->security->xss_clean($this->input->post('eqsl'));
+            $postdata['worked'] = $this->security->xss_clean($this->input->post('worked'));
+            $postdata['confirmed'] = $this->security->xss_clean($this->input->post('confirmed'));
+            $postdata['notworked'] = $this->security->xss_clean($this->input->post('notworked'));
+            $postdata['band'] = $this->security->xss_clean($this->input->post('band'));
+			$postdata['mode'] = $this->security->xss_clean($this->input->post('mode'));
+        }
+        else { // Setting default values at first load of page
+            $postdata['qsl'] = 1;
+            $postdata['lotw'] = 1;
+            $postdata['eqsl'] = 0;
+            $postdata['worked'] = 1;
+            $postdata['confirmed'] = 1;
+            $postdata['notworked'] = 1;
+            $postdata['band'] = 'All';
+			$postdata['mode'] = 'All';
+        }
+
+        $data['rac_array'] = $this->rac->get_rac_array($bands, $postdata);
+        $data['rac_summary'] = $this->rac->get_rac_summary($bands, $postdata);
+
+        // Render Page
+        $data['page_title'] = "Awards - RAC";
+        $this->load->view('interface_assets/header', $data);
+        $this->load->view('awards/rac/index');
+        $this->load->view('interface_assets/footer', $footerData);
+    }
+
     public function iota ()	{
         $this->load->model('iota');
 		$this->load->model('modes');
@@ -912,6 +971,60 @@ class Awards extends CI_Controller {
 
 
         foreach ($was_array as $was => $value) {
+            foreach ($value  as $key) {
+                if($key != "") {
+                    if (strpos($key, '>W<') !== false) {
+                        $states[$was] = 'W';
+                        break;
+                    }
+                    if (strpos($key, '>C<') !== false) {
+                        $states[$was] = 'C';
+                        break;
+                    }
+                    if (strpos($key, '-') !== false) {
+                        $states[$was] = '-';
+                        break;
+                    }
+                }
+            }
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode($states);
+    }
+
+	/*
+        function RAC_map
+
+        This displays the RAC map and requires the $band_type and $mode_type
+    */
+    public function rac_map() {
+		$stateString = 'AB,BC,MB,NB,NL,NT,NS,NU,ON,PE,QC,SK,YT';
+		$racArray = explode(',', $stateString);
+
+        $this->load->model('rac');
+
+		$bands[] = $this->security->xss_clean($this->input->post('band'));
+
+        $postdata['qsl'] = $this->input->post('qsl') == 0 ? NULL: 1;
+        $postdata['lotw'] = $this->input->post('lotw') == 0 ? NULL: 1;
+        $postdata['eqsl'] = $this->input->post('eqsl') == 0 ? NULL: 1;
+        $postdata['worked'] = $this->input->post('worked') == 0 ? NULL: 1;
+        $postdata['confirmed'] = $this->input->post('confirmed')  == 0 ? NULL: 1;
+        $postdata['notworked'] = $this->input->post('notworked')  == 0 ? NULL: 1;
+        $postdata['band'] = $this->security->xss_clean($this->input->post('band'));
+        $postdata['mode'] = $this->security->xss_clean($this->input->post('mode'));
+
+        $rac_array = $this->rac->get_rac_array($bands, $postdata);
+
+        $states = array();
+
+		foreach ($racArray as $state) {                  	 // Generating array for use in the table
+            $states[$state] = '-';                   // Inits each state's count
+        }
+
+
+        foreach ($rac_array as $was => $value) {
             foreach ($value  as $key) {
                 if($key != "") {
                     if (strpos($key, '>W<') !== false) {
