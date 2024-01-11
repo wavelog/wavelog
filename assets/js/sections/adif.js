4aa2152a4e3bf0684eb36aaf65300daea7d147e4
int2001 <joerg@dj7nt.de>
2024-01-11 14:52:37 +0000

    JS-Part of OTF-ZIP

--- assets/js/sections/adif.js
+++ assets/js/sections/adif.js
@@ -1,35 +1,35 @@
 $(document).ready(function(){
 	$('#prepare_sub').click(function(e){
 		e.preventDefault();
-
-		/* Make a zip file here */
-
 		var fi = document.getElementById("userfile");
 		var file = fi.files[0];;
+		if (JSZip.support.blob) {	// Check if Browser supports ZIP
 		var zip = new JSZip();
+			//add all files to zip 
+			addFileToZip(file);
+			function addFileToZip(n) {
+				var arrayBuffer;
+				var fileReader = new FileReader();
+				fileReader.onloadend = function() {
+					arrayBuffer = this.result;
+					zip.file(file.name, arrayBuffer, { binary:true });
+					zip.generateAsync({type:"blob", compression:"DEFLATE"}).then(function(content){
 
-		//add all files to zip 
-		addFileToZip(file);
-		function addFileToZip(n) {
-			var arrayBuffer;
-			var fileReader = new FileReader();
-			fileReader.onloadend = function() {
-				arrayBuffer = this.result;
-				zip.file(file.name, arrayBuffer, { binary:true });
-				zip.generateAsync({type:"blob", compression:"DEFLATE"}).then(function(content){
+						//generated zip content to file type
+						var files = new File([content], file.name + ".zip");
 
-					//generated zip content to file type
-					var files = new File([content], file.name);
-
-					const dataTransfer = new DataTransfer();
-    					dataTransfer.items.add(files);
-					//send generated file to server
-					fi.files=dataTransfer.files;
-					$("#upform").submit();
-					return;
-				});
-			};
-			fileReader.readAsArrayBuffer(file);
+						const dataTransfer = new DataTransfer();
+						dataTransfer.items.add(files);
+						//send generated file to server
+						fi.files=dataTransfer.files;
+						$("#upform").submit();
+						return;
+					});
+				};
+				fileReader.readAsArrayBuffer(file);
+			}
+		} else {
+			$("#upform").submit();
 		}
 	});
 	$('#markExportedToLotw').click(function(e){
