8cdfd06506c31803865f7772f6ef8ecec3883463
int2001 <joerg@dj7nt.de>
2024-01-17 15:58:50 +0000

    Clever... classloader was based upon LENGTH of Productname. Wave is 1 char shorter than Cloud

--- application/helpers/psr4_autoloader_helper.php
+++ application/helpers/psr4_autoloader_helper.php
@@ -7,10 +7,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * Extremely simple autoloader helper, has lots of shortcomings
  */
 spl_autoload_register(function ($class) {
-	if (mb_substr($class, 0, 9) !== 'Wavelog\\') {
+	if (mb_substr($class, 0, 8) !== 'Wavelog\\') {
 		return false;
 	}
-	$class=substr($class,9);
+	$class=substr($class,8);
 	$file = str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php';
 	$file = __DIR__ . "/../../src/" . $file;
 	if (file_exists($file)) {
