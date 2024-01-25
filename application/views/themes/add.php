c6dfeba3b33d9ccfd8f87cb4beadf93ddc937e82
HB9HIL <fabian.berg@hb9hil.org>
2024-01-25 05:09:58 +0100

    adjusted dialogs

--- application/views/themes/add.php
+++ application/views/themes/add.php
@@ -1,24 +1,4 @@
-
-<div class="container" id="create_mode">
-
-	<br>
-	<?php if($this->session->flashdata('message')) { ?>
-		<!-- Display Message -->
-		<div class="alert-message error">
-			<p><?php echo $this->session->flashdata('message'); ?></p>
-		</div>
-	<?php } ?>
-
-	<?php if($this->session->flashdata('notice')) { ?>
-		<div id="message" >
-			<?php echo $this->session->flashdata('notice'); ?>
-		</div>
-	<?php } ?>
-
-	<?php $this->load->helper('form'); ?>
-
-	<?php echo validation_errors(); ?>
-
+<div class="container">
 	<form>
 		<div class="mb-3">
 			<label for="nameInput">Theme Name</label>
@@ -38,7 +18,19 @@
 				<option value="light">Light</option>
 				<option value="dark">Dark</option>
 			</select>
-			<small id="themeModeInputHelp" class="form-text text-muted">This defines wherever the theme is a light or a dark one. On this basis the Logo is chosen.</small>
+			<small id="themeModeInputHelp" class="form-text text-muted">This defines wherever the theme is a light or a dark one.</small>
+		</div>
+
+		<div class="mb-3">
+			<label for="headerLogoInput">Header Logo</label>
+			<input type="text" class="form-control" name="header_logo" id="headerLogoInput" aria-describedby="headerLogoInputHelp">
+			<small id="headerLogoInputHelp" class="form-text text-muted">This is the name of the file which is used as <u>small</u> Logo in the header placed in assets/logo.<br>Only PNG files with a size ratio of 1:1 are allowed.</small>
+		</div>
+
+		<div class="mb-3">
+			<label for="mainLogoInput">Main Logo</label>
+			<input type="text" class="form-control" name="main_logo" id="mainLogoInput" aria-describedby="mainLogoInputHelp">
+			<small id="mainLogoInputHelp" class="form-text text-muted">This is the name of the file which is used as <u>big</u> Logo in the login screen placed in assets/logo.<br>Only PNG files with a size ratio of 1:1 are allowed.</small>
 		</div>
 
 		<button type="button" onclick="addTheme(this.form);" class="btn btn-primary"><i class="fas fa-plus-square"></i> Add theme</button>
