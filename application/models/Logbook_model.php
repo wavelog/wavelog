feceec3fb023846cbfa0595a5b1627c28c23e11e
int2001 <joerg@dj7nt.de>
2024-01-30 06:30:20 +0000

    Added keyauth and flags to lookup-api and removed bugs

--- application/models/Logbook_model.php
+++ application/models/Logbook_model.php
@@ -1281,7 +1281,7 @@ class Logbook_model extends CI_Model {
   *
   */
   function call_lookup_result($callsign) {
-    $this->db->select('COL_CALL, COL_NAME, COL_QSL_VIA, COL_GRIDSQUARE, COL_QTH, COL_IOTA, COL_TIME_ON, COL_STATE, COL_CNTY');
+    $this->db->select('COL_CALL, COL_NAME, COL_QSL_VIA, COL_GRIDSQUARE, COL_QTH, COL_IOTA, COL_TIME_ON, COL_STATE, COL_CNTY, COL_DXCC');
     $this->db->where('COL_CALL', $callsign);
     $where = "COL_NAME != \"\"";
 
