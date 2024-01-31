056ee6bb4a7111eee005f33255431fe85d280080
abarrau <hesky378@gmail.com>
2024-01-31 22:35:48 +0100

    config file for directory userdata

--- application/config/config.sample.php
+++ application/config/config.sample.php
@@ -663,3 +663,12 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+/*
+|--------------------------------------------------------------------------
+| Users Data Directory Options
+|--------------------------------------------------------------------------
+|
+|	'userdatadir'		Name of principal/centralized data stockage
+*/
+$config['userdatadir'] = "userdatadir";
