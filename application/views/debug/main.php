ffcb3e92d0ff45ad7a7fdb2f4abb89676e1799a7
Andreas <6977712+AndreasK79@users.noreply.github.com>
2024-02-02 09:28:49 +0100

    [Debug] Added information about when files were last updated, and links to update

--- application/views/debug/main.php
+++ application/views/debug/main.php
@@ -269,6 +269,54 @@
             </div>
         </div>
         <?php } ?>
+		<div class="card">
+            <div class="card-header">File download date</div>
+            <div class="card-body">
+                <table width="100%" class = "table-sm table table-hover table-striped">
+					<thead>
+						<th>File</th>
+						<th>Last update</th>
+						<th></th>
+					</thead>
+					<tr>
+						<td>DXCC update from Club Log</td>
+                        <td><?php echo $this->optionslib->get_option('dxcc_clublog_update') ?></td>
+						<td><a class="btn btn-sm btn-primary" href="<?php echo site_url('update');?>">Update</a></td>
+
+                    </tr>
+					<tr>
+						<td>DOK file download</td>
+                        <td><?php echo $this->optionslib->get_option('dok_file_update') ?></td>
+						<td><a class="btn btn-sm btn-primary" href="<?php echo site_url('update/update_dok');?>">Update</a></td>
+                    </tr>
+					<tr>
+						<td>LoTW users download</td>
+						<td><?php echo $this->optionslib->get_option('lotw_users_update') ?></td>
+						<td><a class="btn btn-sm btn-primary" href="<?php echo site_url('update/lotw_users');?>">Update</a></td>
+					</tr>
+					<tr>
+                        <td>POTA file download</td>
+                        <td><?php echo $this->optionslib->get_option('pota_file_update') ?></td>
+						<td><a class="btn btn-sm btn-primary" href="<?php echo site_url('update/update_pota');?>">Update</a></td>
+                    </tr>
+					<tr>
+						<td>SCP file download</td>
+                        <td><?php echo $this->optionslib->get_option('scp_update') ?></td>
+						<td><a class="btn btn-sm btn-primary" href="<?php echo site_url('update/update_clublog_scp');?>">Update</a></td>
+                    </tr>
+					<tr>
+						<td>SOTA file download</td>
+						<td><?php echo $this->optionslib->get_option('sota_file_update') ?></td>
+						<td><a class="btn btn-sm btn-primary" href="<?php echo site_url('update/update_sota');?>">Update</a></td>
+					</tr>
+					<tr>
+                        <td>WWFF file downlaod</td>
+                        <td><?php echo $this->optionslib->get_option('wwff_file_update') ?></td>
+						<td><a class="btn btn-sm btn-primary" href="<?php echo site_url('update/update_wwff');?>">Update</a></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
         <?php } ?>
     </div>
 </div>
