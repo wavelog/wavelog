56748ead78026e23f211a5abd3a768bbfae9d48e
int2001 <joerg@dj7nt.de>
2024-02-08 09:47:11 +0000

    Added View/controller

---
+++ application/controllers/Cfdexport.php
@@ -0,0 +1,99 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/* 
+
+	Provides outputted cdf files for use with Google Map services 
+	All maps are stored within /cdf in the root directory
+
+*/
+
+class Kmlexport extends CI_Controller {
+
+    public function index() {
+        $this->load->model('user_model');
+        $this->load->model('modes');
+        $this->load->model('logbook_model');
+	$this->load->model('bands');
+
+        if(!$this->user_model->authorize(2)) { $this->session->set_flashdata('notice', 'You\'re not allowed to do that!'); redirect('dashboard'); }
+
+        $data['worked_bands'] = $this->bands->get_worked_bands(); // Used in the view for band select
+        $data['modes'] = $this->modes->active(); // Used in the view for mode select
+        $data['dxcc'] = $this->logbook_model->fetchDxcc(); // Used in the view for dxcc select
+
+        $data['page_title'] = "KML Export";
+
+        $this->load->view('interface_assets/header', $data);
+        $this->load->view('cdf/index');
+        $this->load->view('interface_assets/footer');
+    }
+
+	public function export() {
+        $this->load->model('user_model');
+        if(!$this->user_model->authorize(2)) { $this->session->set_flashdata('notice', 'You\'re not allowed to do that!'); redirect('dashboard'); }
+		// Load Libraries
+		$this->load->library('qra');
+		$this->load->helper('file');
+
+		// Load Database connections
+		$this->load->model('logbook_model');
+
+		// Parameters
+        $band = $this->input->post('band');
+        $mode = $this->input->post('mode');
+        $dxcc = $this->input->post('dxcc_id');
+        $cqz = $this->input->post('cqz');
+        $propagation = $this->input->post('prop_mode');
+        $fromdate = $this->input->post('from');
+        $todate = $this->input->post('to');
+
+		// Get QSOs with Valid QRAs
+		$qsos = $this->logbook_model->cdf_get_all_qsos($band, $mode, $dxcc, $cqz, $propagation, $fromdate, $todate);
+
+		$output = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+		$output .= "<cdf xmlns=\"http://www.opengis.net/cdf/2.2\">";
+
+		$output .= "<Document>";
+
+		foreach ($qsos->result() as $row) {
+			if($row->COL_GRIDSQUARE != null) {
+				$stn_loc = $this->qra->qra2latlong($row->COL_GRIDSQUARE);
+				
+				$lat = $stn_loc[0];
+				$lng = $stn_loc[1];
+			} else {
+				$query = $this->db->query('
+					SELECT *
+					FROM dxcc_entities
+					WHERE prefix = SUBSTRING( \''.$row->COL_CALL.'\', 1, LENGTH( prefix ) )
+					ORDER BY LENGTH( prefix ) DESC
+					LIMIT 1 
+				');
+					foreach ($query->result() as $dxcc) {
+						$lat = $dxcc->lat;
+						$lng = $dxcc->long;
+					}
+			}
+
+			if (isset($lat) && isset($lng)) {
+				$output .= "<Placemark>";
+
+				$timestamp = strtotime($row->COL_TIME_ON); 
+
+				$output .= "<name>".$row->COL_CALL."</name>";
+				$output .= "<description><![CDATA[<p>Date/Time: ".date('Y-m-d H:i:s', ($timestamp))."<br/>Band: ".$row->COL_BAND."<br /></p>]]></description>";		
+				$output .= "<Point>";
+				$output .= "<coordinates>".$lng.",".$lat.",0</coordinates>";
+				$output .= "</Point>";
+				$output .= "</Placemark>";
+			}
+		}
+
+		$output .= "</Document>";
+		$output .= "</cdf>";
+
+		header("Content-Disposition: attachment; filename=\"qsos.cdf\"");
+		echo $output;
+
+	}
+}
---
+++ application/views/cdf/index.php
@@ -0,0 +1,117 @@
+<div class="container">
+<br>
+    <h2><?php echo lang('export_kml_header'); ?></h2>
+
+    <div class="card">
+        <div class="card-header">
+            <?php echo lang('export_kml_description'); ?>
+        </div>
+
+        <div class="alert alert-warning" role="alert">
+            <?php echo lang('export_kml_grisquare_warning'); ?>
+        </div>
+
+        <div class="card-body">
+
+            <form class="form" action="<?php echo site_url('kmlexport/export'); ?>" method="post" enctype="multipart/form-data">
+                <div class="row">
+                    <div class="mb-3 col-md-3">
+                        <label for="band"><?php echo lang('gen_hamradio_band'); ?></label>
+                            <select id="band" name="band" class="form-select">
+                                <option value="All" <?php if ($this->input->post('band') == "All" || $this->input->method() !== 'post') echo ' selected'; ?> ><?php echo lang('general_word_all'); ?></option>
+                                <?php foreach($worked_bands as $band) {
+                                    echo '<option value="' . $band . '"';
+                                    if ($this->input->post('band') == $band) echo ' selected';
+                                    echo '>' . $band . '</option>'."\n";
+                                } ?>
+                            </select>
+                    </div>
+
+                    <div class="mb-3 col-md-3">
+                    <label for="mode"><?php echo lang('gen_hamradio_mode'); ?></label>
+                        <select id="mode" name="mode" class="form-select">
+                            <option value="All"><?php echo lang('general_word_all'); ?></option>
+                            <?php
+                            foreach($modes->result() as $mode){
+                                if ($mode->submode == null) {
+                                    echo '<option value="' . $mode->mode . '">'. $mode->mode . '</option>'."\n";
+                                } else {
+                                    echo '<option value="' . $mode->submode . '">' . $mode->submode . '</option>'."\n";
+                                }
+                            }
+                            ?>
+                        </select>
+                    </div>
+
+                    <div class="mb-3 col-md-4">
+                        <label for="dxcc_id"><?php echo lang('gen_hamradio_dxcc'); ?></label>
+                        <select class="form-select" id="dxcc_id" name="dxcc_id">
+                            <option value="All"><?php echo lang('general_word_all'); ?></option>
+                            <?php
+                                foreach($dxcc as $d){
+                                    echo '<option value=' . $d->adif . '>' . $d->prefix . ' - ' . ucwords(strtolower($d->name), "- (/");
+                                    if ($d->Enddate != null) {
+                                        echo ' ('.lang('gen_hamradio_deleted_dxcc').')';
+                                    }
+                                    echo '</option>';
+                                }
+                            ?>
+
+                        </select>
+                    </div>
+                </div>
+                <div class="row">                
+                    <div class="mb-3 col-md-3">
+                        <label for="cqz"><?php echo lang('gen_hamradio_cq_zone'); ?></label>
+                        <select class="form-select" id="cqz" name="cqz">
+                            <option value="All"><?php echo lang('general_word_all'); ?></option>
+                            <?php
+                            for ($i = 1; $i<=40; $i++) {
+                                echo '<option value="'. $i . '">'. $i .'</option>';
+                            }
+                            ?>
+                        </select>
+                    </div>
+
+                    <div class="mb-3 col-md-5">
+                        <label for="selectPropagation"><?php echo lang('gen_hamradio_propagation_mode'); ?></label>
+                        <select class="form-select" id="selectPropagation" name="prop_mode">
+                            <option value="All"><?php echo lang('general_word_all'); ?></option>
+                            <option value="AS">Aircraft Scatter</option>
+                            <option value="AUR">Aurora</option>
+                            <option value="AUE">Aurora-E</option>
+                            <option value="BS">Back scatter</option>
+                            <option value="ECH">EchoLink</option>
+                            <option value="EME">Earth-Moon-Earth</option>
+                            <option value="ES">Sporadic E</option>
+                            <option value="FAI">Field Aligned Irregularities</option>
+                            <option value="F2">F2 Reflection</option>
+                            <option value="INTERNET">Internet-assisted</option>
+                            <option value="ION">Ionoscatter</option>
+                            <option value="IRL">IRLP</option>
+                            <option value="MS">Meteor scatter</option>
+                            <option value="RPT">Terrestrial or atmospheric repeater or transponder</option>
+                            <option value="RS">Rain scatter</option>
+                            <option value="SAT">Satellite</option>
+                            <option value="TEP">Trans-equatorial</option>
+                            <option value="TR">Tropospheric ducting</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="mb-3 col-md-3">
+                        <label for="from"><?php echo lang('gen_from_date') . ": " ?></label>
+                        <input name="from" id="from" type="date" class="form-control w-auto">
+                    </div>
+
+                    <div class="mb-3 col-md-3">
+                        <label for="to"><?php echo lang('gen_to_date') . ": " ?></label>
+                        <input name="to" id="to" type="date" class="form-control w-auto">
+                    </div>
+                </div>    
+                <br>
+                <button type="submit" class="btn btn-primary mb-2" value="Export"><?php echo lang('general_word_export'); ?></button>
+            </form>
+        </div>
+    </div>
+</div>
