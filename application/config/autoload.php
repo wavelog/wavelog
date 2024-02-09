1a921fe32f86ba7255db7252c29d2137aa52c69f
HB9HIL <fabian.berg@hb9hil.org>
2024-02-09 11:04:50 +0100

    added models which are already loaded everywhere

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -127,7 +127,7 @@ $autoload['language'] = array(
 |
 */
 
-$autoload['model'] = array('user_options_model');
+$autoload['model'] = array('user_options_model', 'logbooks_model', 'stations', 'oqrs_model');
 
 
 /* End of file autoload.php */
