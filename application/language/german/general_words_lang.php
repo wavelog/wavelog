c4e390d5001e1cf87af85027dc2693c7b4d1780c
HB9HIL <fabian.berg@hb9hil.org>
2024-02-14 12:45:34 +0100

    lang 'active'

--- application/language/german/general_words_lang.php
+++ application/language/german/general_words_lang.php
@@ -28,6 +28,8 @@ $lang['general_word_filtering_on'] = "Filtern auf";
 $lang['general_word_not_display'] = "Not display";
 $lang['general_word_icon'] = "Icon";
 $lang['general_word_never'] = "Nie";
+$lang['general_word_active'] = "Active";
+$lang['general_word_inactive'] = "Inactive";
 
 $lang['general_word_date'] = 'Datum';
 $lang['general_word_startdate'] = "Start Datum";
