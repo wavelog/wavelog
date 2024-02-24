4a07d3073c987c9a83afa916782bb8e4c2676627
HB9HIL <fabian.berg@hb9hil.org>
2024-02-24 10:23:58 +0100

    moved folderSize und sizeFormat functions to libary

--- application/controllers/Qsl.php
+++ application/controllers/Qsl.php
@@ -17,8 +17,9 @@ class Qsl extends CI_Controller {
     public function index() {
 
         $this->load->model('qsl_model');
+        $this->load->library('Genfunctions');
         $folder_name = $this->qsl_model->get_imagePath('p');
-        $data['storage_used'] = sizeFormat(folderSize($folder_name));
+        $data['storage_used'] = $this->genfunctions->sizeFormat($this->genfunctions->folderSize($folder_name));
 
         // Render Page
         $data['page_title'] = "QSL Cards";
@@ -173,48 +174,4 @@ class Qsl extends CI_Controller {
         $this->load->view('qslcard/qslcarousel', $data);
     }
 
-}
-
-// Functions for storage, these need shifted to a libary to use across Wavelog
-function folderSize($dir){
-    $count_size = 0;
-    $count = 0;
-    $dir_array = scandir($dir);
-      foreach($dir_array as $key=>$filename){
-        if($filename!=".." && $filename!="."){
-           if(is_dir($dir."/".$filename)){
-              $new_foldersize = foldersize($dir."/".$filename);
-              $count_size = $count_size+ $new_foldersize;
-            }else if(is_file($dir."/".$filename)){
-              $count_size = $count_size + filesize($dir."/".$filename);
-              $count++;
-            }
-       }
-     }
-    return $count_size;
-}
-
-function sizeFormat($bytes){
-    $kb = 1024;
-    $mb = $kb * 1024;
-    $gb = $mb * 1024;
-    $tb = $gb * 1024;
-
-    if (($bytes >= 0) && ($bytes < $kb)) {
-    return $bytes . ' B';
-
-    } elseif (($bytes >= $kb) && ($bytes < $mb)) {
-    return ceil($bytes / $kb) . ' KB';
-
-    } elseif (($bytes >= $mb) && ($bytes < $gb)) {
-    return ceil($bytes / $mb) . ' MB';
-
-    } elseif (($bytes >= $gb) && ($bytes < $tb)) {
-    return ceil($bytes / $gb) . ' GB';
-
-    } elseif ($bytes >= $tb) {
-    return ceil($bytes / $tb) . ' TB';
-    } else {
-    return $bytes . ' B';
-    }
 }
