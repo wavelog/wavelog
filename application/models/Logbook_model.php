88aaa753b8268d324e6a2e24768931e24afda084
HB9HIL <fabian.berg@hb9hil.org>
2024-03-08 10:02:04 +0100

    additional fix layer; only show one result

--- application/models/Logbook_model.php
+++ application/models/Logbook_model.php
@@ -1678,7 +1678,8 @@ class Logbook_model extends CI_Model {
 		  $this->db->join('lotw_users', $this->config->item('table_name').'.COL_CALL = lotw_users.callsign', 'left outer');
 		  $this->db->join('primary_subdivisions', $this->config->item('table_name').'.COL_DXCC = primary_subdivisions.adif AND '.$this->config->item('table_name').'.COL_STATE = primary_subdivisions.state', 'left outer');
 		  $this->db->where('COL_PRIMARY_KEY', $id);
-
+      $this->db->limit(1);
+      
 		  return $this->db->get();
 	  } else {
 		  return;
