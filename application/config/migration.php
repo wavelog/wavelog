75f9ddc2dd3379f68428af3f89722d73f8cc5c3c
int2001 <joerg@dj7nt.de>
2024-03-22 06:51:36 +0000

    Added Migration for those Users coming from myISAM

--- application/config/migration.php
+++ application/config/migration.php
@@ -22,7 +22,7 @@ $config['migration_enabled'] = TRUE;
 |
 */
 
-$config['migration_version'] = 189;
+$config['migration_version'] = 190;
 
 /*
 |--------------------------------------------------------------------------
