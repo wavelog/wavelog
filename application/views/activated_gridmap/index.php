d86474c2ceec2636c0188916e17cc38eb7271981
phl0 <github@florian-wolters.de>
2024-04-08 15:03:32 +0200

    Add orbit type to activated gridsquares

--- application/views/activated_gridmap/index.php
+++ application/views/activated_gridmap/index.php
@@ -27,6 +27,15 @@
             <?php } else { ?>
                 <input id="sats" type="hidden" value="All"></input>
             <?php } ?>
+                <label class="my-1 me-2" id="orbitslabel" for="orbits" <?php if ($user_default_band != "SAT") { ?>style="display: none;"<?php } ?>><?php echo lang('gridsquares_orbit'); ?></label>
+                <select class="form-select my-1 me-sm-2 w-auto"  id="orbits" <?php if ($user_default_band != "SAT") { ?>style="display: none;"<?php } ?>>
+                    <option value="All"><?php echo lang('general_word_all')?></option>
+                    <?php
+                    foreach($orbits as $orbit){
+                         echo '<option value="' . $orbit . '">' . strtoupper($orbit) . '</option>'."\n";
+                    }
+                    ?>
+            </select>
 			<label class="my-1 me-2" for="mode"><?php echo lang('gridsquares_mode'); ?></label>
             <select class="form-select my-1 me-sm-2 w-auto"  id="mode">
 			<option value="All"><?php echo lang('general_word_all')?></option>
