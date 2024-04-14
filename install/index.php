5beb7f2d146135924f1190772fe2a74081a7fe4e
Christoph Kottke <dg0tm@darc.de>
2024-04-14 10:30:53 +0200

    [installer] Database Tap fixes

--- install/index.php
+++ install/index.php
@@ -438,7 +438,7 @@ global $wavelog_url;
 											<div class="col">
 												<div class="mb-3">
 													<label for="db_hostname" class="form-label">Hostname or IP<i id="callbook_tooltip" data-bs-toggle="tooltip" data-bs-placement="top" title="Directory Hint" class="fas fa-question-circle text-muted ms-2" data-bs-custom-class="custom-tooltip" data-bs-html="true" data-bs-title="Usually 'localhost'. Optional with '...:[port]'. Default Port: 3306"></i></label>
-													<input type="text" id="db_hostname" value="localhost" class="form-control" name="db_hostname" />
+													<input type="text" id="db_hostname" placeholder="localhost" class="form-control" name="db_hostname" />
 												</div>
 											</div>
 											<div class="col">
