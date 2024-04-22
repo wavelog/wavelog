cdbaefb42daf24cda252704242fafefc963fe231
HB9HIL <fabian.berg@hb9hil.org>
2024-04-23 01:46:26 +0200

    only admins are allowed to enter cron page

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -17,7 +17,7 @@ class cron extends CI_Controller
 	public function index() {
 
         $this->load->model('user_model');
-		if(!$this->user_model->authorize(2)) { $this->session->set_flashdata('notice', 'You\'re not allowed to do that!'); redirect('dashboard'); }
+		if(!$this->user_model->authorize(99)) { $this->session->set_flashdata('notice', 'You\'re not allowed to do that!'); redirect('dashboard'); }
 
 		$this->load->helper('file');
 
