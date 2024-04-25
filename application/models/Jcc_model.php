0ace9a48d1e5c7d72bfec60d3139f03df66bb9e9
phl0 <github@florian-wolters.de>
2024-04-25 07:37:18 +0200

    Refactor model loading

--- application/models/Jcc_model.php
+++ application/models/Jcc_model.php
@@ -825,8 +825,7 @@ class Jcc_model extends CI_Model {
 	);
 
 	function get_jcc_array($bands, $postdata) {
-		$CI =& get_instance();
-		$CI->load->model('logbooks_model');
+		$this->load->model('logbooks_model');
 		$logbooks_locations_array = $CI->logbooks_model->list_logbook_relationships($this->session->userdata('active_station_logbook'));
 
 		if (!$logbooks_locations_array) {
