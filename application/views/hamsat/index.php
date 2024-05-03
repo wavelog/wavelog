8faabd7bf93d54f34fb15d6fcf6a53754b0f09d3
phl0 <github@florian-wolters.de>
2024-05-03 13:35:16 +0200

    Fix view if feed key is not set (yet)

--- application/views/hamsat/index.php
+++ application/views/hamsat/index.php
@@ -2,24 +2,27 @@
     <br>
     <h2>Hamsat - Satellite Rovers</h2>
     <p>This data is from <a target="_blank" href="https://hams.at/">https://hams.at/</a>.
-    <span id="workable_hint">
-    <?php if ($user_hamsat_workable_only == '1') {
-       echo " Only workable passes shown.";
-    } else {
-       echo " All passes shown.";
-    }?>
-    </span>
     <script type="text/javascript">
        var workable_preset = <?php echo $user_hamsat_workable_only; ?>;
+       var feed_key_set = <?php echo strlen($user_hamsat_key); ?>;
     </script>
-       <button id="toggle_workable" value="<?php echo $user_hamsat_workable_only ? '0' : '1'; ?>" type="button" onclick="loadHamsAt(this);" class="btn btn-info btn-sm"><?php echo $user_hamsat_workable_only ? 'Show all passes' : 'Show workable passes only'; ?></button>
+    <?php if ($user_hamsat_key != '') { ?>
+    <span id="workable_hint">
+       <?php if ($user_hamsat_workable_only == '1') {
+          echo " Only workable passes shown.";
+       } else {
+          echo " All passes shown.";
+       } ?>
+    </span>
+    <button id="toggle_workable" value="<?php echo $user_hamsat_workable_only ? '0' : '1'; ?>" type="button" onclick="loadHamsAt(this);" class="btn btn-info btn-sm"><?php echo $user_hamsat_workable_only ? 'Show all passes' : 'Show workable passes only'; ?></button>
+    <?php } ?>
     </p>
     <p>
     <?php if ($user_hamsat_workable_only && $user_hamsat_key == '') { ?>
     <div class="alert alert-warning" role="warning">
-       Private feed key empty. Please set the feed key in your profile.
+       Cannot filter workable passes only without private feed key. Please set the feed key in your profile.
     </div>
-    <?php } else { ?>
+    <?php } ?>
     <table style="width:100%" class="table table-hover table-striped" id="activationsList">
     <thead>
         <tr>
@@ -38,5 +41,4 @@
     <tbody>
     </tbody>
 </table>
-    <?php } ?>
 </div>
