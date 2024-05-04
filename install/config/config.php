00c2bccfdda7252362b77ae6c848812e6e6b8ef9
HB9HIL <fabian.berg@hb9hil.org>
2024-05-04 09:51:41 +0200

    changed comment

--- install/config/config.php
+++ install/config/config.php
@@ -686,7 +686,6 @@ $config['userdata'] = 'userdata';
 |	'cron_ip'		List of IP Adresses / Networks which are allowed to run the mastercron job 'cron/run'
 |
 |   Type in the IP Addresses including subnet. If more then one seperate with comma e.g. 192.168.0.0/24,192.168.1.0/24
-| 	To allow only the localhost to run the mastercron type '127.0.0.1'. 
 |	'0.0.0.0' means all IP's are allowed.
 |
 */
