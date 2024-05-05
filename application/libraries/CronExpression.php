4881173a80c376d350df099a84dc0b23a9290793
HB9HIL <fabian.berg@hb9hil.org>
2024-05-05 11:57:03 +0200

    support for @ expression (PHP)

--- application/libraries/CronExpression.php
+++ application/libraries/CronExpression.php
@@ -69,6 +69,17 @@ class CronExpression {
         ]
     ];
 
+    private const SPECIAL_EXPRESSIONS = [
+        '@yearly' => '0 0 1 1 *',
+        '@annually' => '0 0 1 1 *',
+        '@monthly' => '0 0 1 * *',
+        '@weekly' => '0 0 * * 0',
+        '@daily' => '0 0 * * *',
+        '@midnight' => '0 0 * * *',
+        '@hourly' => '0 * * * *'
+    ];
+    
+
     /**
      * @var DateTimeZone|null
      */
@@ -249,8 +260,18 @@ class CronExpression {
             }
 
             return $registers;
-        }
 
+        } else if (strpos($expression, '@') === 0) {
+
+            $special = trim($expression);
+
+            if (isset(self::SPECIAL_EXPRESSIONS[$special])) {
+
+                $special_expression = self::SPECIAL_EXPRESSIONS[$special];
+                return $this->parse($special_expression);
+
+            }
+        }
         throw new Exception('invalid number of segments');
     }
 
