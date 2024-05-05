674b4cf3c4a542e5a737a997d49fbf6fe7da5034
int2001 <joerg@dj7nt.de>
2024-05-05 15:19:20 +0000

    Fixes UTC at header (show when enabled, even if Quickswitch was disabled)

--- application/views/interface_assets/header.php
+++ application/views/interface_assets/header.php
@@ -467,15 +467,14 @@
 									<li><a class="dropdown-item" href="<?php echo site_url('stationsetup'); ?>" title="Manage station locations"><?php echo lang('menu_station_setup'); ?>...</a></li>
 								</ul>
 							</li>
-							<?php
+							<?php }
 							$utc_headermenu = ($this->user_options_model->get_options('header_menu', array('option_name' => 'utc_headermenu'))->row()->option_value ?? 'false');
 							if ($utc_headermenu == 'true') { ?>
 								<li class="nav-link disabled">
 									<a id="utc_header" style="width: 70px; display: inline-block;"></a>
 								</li>
 
-							<?php } ?>
-						<?php }
+							<?php }
 
 						// Can add extra menu items by defining them in options. The format is json.
 						// Useful to add extra things in Wavelog without the need for modifying files. If you add extras, these files will not be overwritten when updating.
