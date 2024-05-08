fcf93620e465f8ceb4afd0cfde09e62bb305d639
int2001 <joerg@dj7nt.de>
2024-05-08 18:38:06 +0000

    Cloning of QSO-Up to DOWN

--- application/controllers/Clublog.php
+++ application/controllers/Clublog.php
@@ -36,6 +36,113 @@ class Clublog extends CI_Controller {
 		}
 	}
 
+	function downloadUser($userid, $username, $password) {
+		$clean_username = $this->security->xss_clean($username);
+		$clean_passord = $this->security->xss_clean($password);
+		$clean_userid = $this->security->xss_clean($userid);
+
+		$this->config->load('config');
+		ini_set('memory_limit', '-1');
+		ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL);
+
+		$this->load->helper('file');
+
+		$this->load->model('clublog_model');
+
+		$station_profiles = $this->clublog_model->all_with_count($clean_userid);
+
+		if($station_profiles->num_rows()){
+			foreach ($station_profiles->result() as $station_row)
+			{
+				if($station_row->qso_total > 0) {
+					$data['qsos'] = $this->clublog_model->get_clublog_qsos($station_row->station_id);
+
+					if($data['qsos']->num_rows()){
+						$string = $this->load->view('adif/data/clublog', $data, TRUE);
+
+						$ranid = uniqid();
+
+						if ( ! write_file('uploads/clublog'.$ranid.$station_row->station_id.'.adi', $string)) {
+						     echo 'Unable to write the file - Make the folder Upload folder has write permissions.';
+						}
+						else {
+
+							$file_info = get_file_info('uploads/clublog'.$ranid.$station_row->station_id.'.adi');
+
+							// initialise the curl request
+							$request = curl_init('https://clublog.org/putlogs.php');
+
+							if($this->config->item('directory') != "") {
+								 $filepath = $_SERVER['DOCUMENT_ROOT']."/".$this->config->item('directory')."/".$file_info['server_path'];
+							} else {
+								 $filepath = $_SERVER['DOCUMENT_ROOT']."/".$file_info['server_path'];
+							}
+
+							if (function_exists('curl_file_create')) { // php 5.5+
+							  $cFile = curl_file_create($filepath);
+							} else { //
+							  $cFile = '@' . realpath($filepath);
+							}
+
+							// send a file
+							curl_setopt($request, CURLOPT_POST, true);
+							curl_setopt(
+							    $request,
+							    CURLOPT_POSTFIELDS,
+							    array(
+							      'email' => $clean_username,
+							      'password' => $clean_passord,
+							      'callsign' => $station_row->station_callsign,
+							      'api' => "608df94896cb9c5421ae748235492b43815610c9",
+							      'file' => $cFile
+							    ));
+
+							// output the response
+							curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+							$response = curl_exec($request);
+							$info = curl_getinfo($request);
+
+							if(curl_errno($request)) {
+							    echo curl_error($request);
+							}
+							curl_close ($request);
+
+
+							// If Clublog Accepts mark the QSOs
+							if (preg_match('/\baccepted\b/', $response)) {
+								echo "QSOs uploaded and Logbook QSOs marked as sent to Clublog"."<br>";
+								$this->load->model('clublog_model');
+								$this->clublog_model->mark_qsos_sent($station_row->station_id);
+								echo "Clublog upload for ".$station_row->station_callsign."<br>";
+								log_message('info', 'Clublog upload for '.$station_row->station_callsign.' successfully sent.');
+							} else if (preg_match('/checksum duplicate/',$response)) {
+								echo "QSOs uploaded (asduplicate!) and Logbook QSOs marked as sent to Clublog"."<br>";
+								$this->load->model('clublog_model');
+								$this->clublog_model->mark_qsos_sent($station_row->station_id);
+								echo "Clublog upload for ".$station_row->station_callsign."<br>";
+								log_message('info', 'Clublog DUPLICATE upload for '.$station_row->station_callsign.' successfully sent.');
+							} else {
+								echo "Error ".$response."<br />";
+								log_message('error', 'Clublog upload for '.$station_row->station_callsign.' failed reason '.$response);
+							}
+
+							// Delete the ADIF file used for clublog
+							unlink('uploads/clublog'.$ranid.$station_row->station_id.'.adi');
+
+						}
+
+					} else {
+						echo "Nothing awaiting upload to clublog for ".$station_row->station_callsign."<br>";
+
+						log_message('info', 'Nothing awaiting upload to clublog for '.$station_row->station_callsign);
+					}
+				}
+			}
+		}
+	}
+
 	function uploadUser($userid, $username, $password) {
 		$clean_username = $this->security->xss_clean($username);
 		$clean_passord = $this->security->xss_clean($password);
