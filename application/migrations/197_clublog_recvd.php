290e6ecf9a033b931cb0aa60b8c5a6a79b50e771
int2001 <joerg@dj7nt.de>
2024-05-09 14:03:30 +0000

    Added cronjob to that new cron-table

--- application/migrations/197_clublog_recvd.php
+++ application/migrations/197_clublog_recvd.php
@@ -3,13 +3,25 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Migration_clublog_recvd extends CI_Migration {
 	public function up() {
-                if (!$this->db->field_exists('COL_CLUBLOG_QSO_DOWNLOAD_STATUS', $this->config->item('table_name'))) {
-                        $fields = array(
-                                'COLUMN COL_CLUBLOG_QSO_DOWNLOAD_DATE DATETIME NULL DEFAULT NULL',
-                                'COLUMN COL_CLUBLOG_QSO_DOWNLOAD_STATUS VARCHAR(10) DEFAULT NULL',
-                        );
-                        $this->dbforge->add_column($this->config->item('table_name'), $fields);
+		if (!$this->db->field_exists('COL_CLUBLOG_QSO_DOWNLOAD_STATUS', $this->config->item('table_name'))) {
+			$fields = array(
+				'COLUMN COL_CLUBLOG_QSO_DOWNLOAD_DATE DATETIME NULL DEFAULT NULL',
+				'COLUMN COL_CLUBLOG_QSO_DOWNLOAD_STATUS VARCHAR(10) DEFAULT NULL',
+			);
+			$this->dbforge->add_column($this->config->item('table_name'), $fields);
 		}
+		$data = array(
+			array(
+				'id' => 'clublog_download',
+				'enabled' => '0',
+				'status' => 'pending',
+				'description' => 'Download QSOs from Clublog',
+				'function' => 'index.php/clublog/download',
+				'expression' => '7 00 * * *',
+				'last_run' => null,
+				'next_run' => null
+			));
+			$this->db->insert_batch('cron', $data);
 	}
 
 	public function down() {
