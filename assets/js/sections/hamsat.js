7037b7aabec4e06fbf981bc9fac42700a6fc6e5b
phl0 <github@florian-wolters.de>
2024-05-16 12:58:51 +0200

    Only show and evaluate 4-char grids for hams.at

--- assets/js/sections/hamsat.js
+++ assets/js/sections/hamsat.js
@@ -107,9 +107,9 @@ function loadActivationsTable(rows, show_workable_only) {
 		grids = [];
 		for (var j=0; j < activation.grids_wkd.length; j++) {
 			if (activation.grids_wkd[j] == 1) {
-				grids.push("<span data-bs-toggle=\"tooltip\" title=\"Worked\" class=\"badge bg-success\">"+activation.grids[j]+"</span>")
+				grids.push("<span data-bs-toggle=\"tooltip\" title=\"Worked\" class=\"badge bg-success\">"+activation.grids[j].substring(0, 4)+"</span>")
 			} else {
-				grids.push("<span data-bs-toggle=\"tooltip\" title=\"Not Worked\" class=\"badge bg-danger\">"+activation.grids[j]+"</span>")
+				grids.push("<span data-bs-toggle=\"tooltip\" title=\"Not Worked\" class=\"badge bg-danger\">"+activation.grids[j].substring(0, 4)+"</span>")
 			}
 		}
 		data.push(grids.join(' '));
