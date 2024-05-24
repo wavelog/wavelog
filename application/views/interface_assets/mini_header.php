a086dd2e378e5fda8c58dd0e33e23b8bd7f2c4ea
HB9HIL <fabian.berg@hb9hil.org>
2024-05-24 09:32:09 +0200

    adjusted fontawesome source

--- application/views/interface_assets/mini_header.php
+++ application/views/interface_assets/mini_header.php
@@ -11,7 +11,7 @@
 		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/general.css">
 		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/<?php echo $this->optionslib->get_theme();?>/overrides.css">
 	<?php } ?>
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/fontawesome/css/all.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/fontawesome/css/all.min.css">
 
 	   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/jquery.fancybox.min.css" />
 
