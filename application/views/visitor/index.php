0663640e724ad014426811200dac4b8c9142823c
HB9HIL <fabian.berg@hb9hil.org>
2024-05-27 13:21:33 +0200

    missing tbody

--- application/views/visitor/index.php
+++ application/views/visitor/index.php
@@ -138,10 +138,8 @@ if ($public_maps_option == 'true') { ?>
 	<!-- Log Data -->
 	<div class="row logdata">
 		<div class="col-sm-8">
-
 			<div class="table-responsive">
 				<table class="table table-striped table-hover">
-
 					<thead>
 						<tr class="titles">
 							<th><?php echo lang('general_word_date'); ?></th>
@@ -158,54 +156,53 @@ if ($public_maps_option == 'true') { ?>
 							?>
 						</tr>
 					</thead>
+					<tbody>
+						<?php
+						$i = 0;
+						if (!empty($results) > 0) {
+							foreach ($results->result() as $row) { ?>
+								<?php echo '<tr class="tr' . ($i & 1) . '">'; ?>
 
-					<?php
-					$i = 0;
-					if (!empty($last_five_qsos) > 0) {
-						foreach ($last_five_qsos->result() as $row) { ?>
-							<?php echo '<tr class="tr' . ($i & 1) . '">'; ?>
+								<?php
 
-							<?php
+								// Get Date format
+								if ($this->session->userdata('user_date_format')) {
+									// If Logged in and session exists
+									$custom_date_format = $this->session->userdata('user_date_format');
+								} else {
+									// Get Default date format from /config/wavelog.php
+									$custom_date_format = $this->config->item('qso_date_format');
+								}
 
-							// Get Date format
-							if ($this->session->userdata('user_date_format')) {
-								// If Logged in and session exists
-								$custom_date_format = $this->session->userdata('user_date_format');
-							} else {
-								// Get Default date format from /config/wavelog.php
-								$custom_date_format = $this->config->item('qso_date_format');
-							}
+								?>
 
-							?>
-
-							<td><?php $timestamp = strtotime($row->COL_TIME_ON);
-								echo date($custom_date_format, $timestamp); ?></td>
-							<?php if (($this->config->item('use_auth') && ($this->session->userdata('user_type') >= 2)) || $this->config->item('use_auth') === FALSE || ($this->config->item('show_time'))) { ?>
 								<td><?php $timestamp = strtotime($row->COL_TIME_ON);
-									echo date('H:i', $timestamp); ?></td>
+									echo date($custom_date_format, $timestamp); ?></td>
+								<?php if (($this->config->item('use_auth') && ($this->session->userdata('user_type') >= 2)) || $this->config->item('use_auth') === FALSE || ($this->config->item('show_time'))) { ?>
+									<td><?php $timestamp = strtotime($row->COL_TIME_ON);
+										echo date('H:i', $timestamp); ?></td>
 
-							<?php } ?>
-							<td>
-								<?php echo str_replace("0", "&Oslash;", strtoupper($row->COL_CALL)); ?>
-							</td>
-							<?php
-							echo_table_col($row, $this->session->userdata('user_column1') == "" ? 'Mode' : $this->session->userdata('user_column1'));
-							echo_table_col($row, $this->session->userdata('user_column2') == "" ? 'RSTS' : $this->session->userdata('user_column2'));
-							echo_table_col($row, $this->session->userdata('user_column3') == "" ? 'RSTR' : $this->session->userdata('user_column3'));
-							echo_table_col($row, $this->session->userdata('user_column4') == "" ? 'Band' : $this->session->userdata('user_column4'));
-							?>
-							</tr>
-					<?php $i++;
-						}
-					} ?>
+								<?php } ?>
+								<td>
+									<?php echo str_replace("0", "&Oslash;", strtoupper($row->COL_CALL)); ?>
+								</td>
+								<?php
+								echo_table_col($row, $this->session->userdata('user_column1') == "" ? 'Mode' : $this->session->userdata('user_column1'));
+								echo_table_col($row, $this->session->userdata('user_column2') == "" ? 'RSTS' : $this->session->userdata('user_column2'));
+								echo_table_col($row, $this->session->userdata('user_column3') == "" ? 'RSTR' : $this->session->userdata('user_column3'));
+								echo_table_col($row, $this->session->userdata('user_column4') == "" ? 'Band' : $this->session->userdata('user_column4'));
+								?>
+								</tr>
+						<?php $i++;
+							}
+						} ?>
+					</tbody>
 				</table>
 			</div>
 		</div>
 
 		<div class="col-sm-4">
 			<div class="table-responsive">
-
-
 				<table class="table table-striped">
 					<tr class="titles">
 						<td colspan="2"><i class="fas fa-chart-bar"></i> <?php echo lang('dashboard_qso_breakdown'); ?></td>
@@ -226,8 +223,6 @@ if ($public_maps_option == 'true') { ?>
 						<td width="50%"><?php echo $month_qsos; ?></td>
 					</tr>
 				</table>
-
-
 
 				<table class="table table-striped">
 					<tr class="titles">
@@ -314,7 +309,6 @@ if ($public_maps_option == 'true') { ?>
 			</div>
 		</div>
 	</div>
-
 </div>
 
 <div id="partial_view"></div>
