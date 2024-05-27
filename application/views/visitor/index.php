94251db95a0f306cea153e671621f6556618bfcf
HB9HIL <fabian.berg@hb9hil.org>
2024-05-27 13:23:58 +0200

    added pagination

--- application/views/visitor/index.php
+++ application/views/visitor/index.php
@@ -199,6 +199,33 @@ if ($public_maps_option == 'true') { ?>
 					</tbody>
 				</table>
 			</div>
+			<?php if (isset($this->pagination)) { ?>
+				<?php
+				$config['full_tag_open'] = '<ul class="pagination">';
+				$config['full_tag_close'] = '</ul>';
+				$config['attributes'] = ['class' => 'page-link'];
+				$config['first_link'] = false;
+				$config['last_link'] = false;
+				$config['first_tag_open'] = '<li class="page-item">';
+				$config['first_tag_close'] = '</li>';
+				$config['prev_link'] = '&laquo';
+				$config['prev_tag_open'] = '<li class="page-item">';
+				$config['prev_tag_close'] = '</li>';
+				$config['next_link'] = '&raquo';
+				$config['next_tag_open'] = '<li class="page-item">';
+				$config['next_tag_close'] = '</li>';
+				$config['last_tag_open'] = '<li class="page-item">';
+				$config['last_tag_close'] = '</li>';
+				$config['cur_tag_open'] = '<li class="page-item active"><a href="#" class="page-link">';
+				$config['cur_tag_close'] = '<span class="visually-hidden">(current)</span></a></li>';
+				$config['num_tag_open'] = '<li class="page-item">';
+				$config['num_tag_close'] = '</li>';
+				$this->pagination->initialize($config);
+				?>
+
+				<?php echo $this->pagination->create_links(); ?>
+
+			<?php } ?>
 		</div>
 
 		<div class="col-sm-4">
