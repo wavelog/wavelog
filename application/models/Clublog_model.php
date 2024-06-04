5fa8c29798a735f03eb5205dae85bfb4cdaa2e6e
int2001 <joerg@dj7nt.de>
2024-06-04 04:43:27 +0000

    Whitepacing at that section

--- application/models/Clublog_model.php
+++ application/models/Clublog_model.php
@@ -160,14 +160,14 @@ class Clublog_model extends CI_Model
 				} elseif (preg_match_all('/Login rejected/', $response)) {
 					$return = "Wrong Clublog username and password for Callsign: '" . $station_row->station_callsign . "'. 'LOGIN REJECTED'.";
 					log_message('debug', $return);
-        				$sql = 'update station_profile set clublogignore = 1 where station_id = ?';
-        				$this->db->query($sql,$station_row->station_id);
+					$sql = 'update station_profile set clublogignore = 1 where station_id = ?';
+					$this->db->query($sql,$station_row->station_id);
 				} elseif (preg_match_all('/Invalid callsign/', $response)) {	// We're trying to download calls for a station we're not granted. Disable Clublog-Transfer for that station(s)
 					$this->disable_sync4call($station_row->station_callsign, $station_row->station_ids);
 					$return = "The callsign '" . $station_row->station_callsign . "' does not match the user account at Clublog. 'INVALID CALLSIGN'.";
 					log_message('debug', $return);
-        				$sql = 'update station_profile set clublogignore = 1 where station_id = ?';
-        				$this->db->query($sql,$station_row->station_id);
+					$sql = 'update station_profile set clublogignore = 1 where station_id = ?';
+					$this->db->query($sql,$station_row->station_id);
 				} else {
 					try {
 						$cl_qsls = json_decode($response);
