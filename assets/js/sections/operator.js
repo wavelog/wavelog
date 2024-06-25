1cf9c7db184246548192a8c0602c03733192221a
HB9HIL <fabian.berg@hb9hil.org>
2024-06-25 23:31:07 +0200

    operator.js

---
+++ assets/js/sections/operator.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+	$("#operator_callsign").on("keydown", function (event) {
+		if (event.which == 13) {
+			saveOperator();
+		}
+	});
+});
+
+function displayOperatorDialog() {
+	$.ajax({
+		url: base_url + "index.php/Operator/displayOperatorDialog",
+		type: "GET",
+		dataType: "html",
+		success: function (data) {
+			$("body").append(data);
+
+			var operatorModal = new bootstrap.Modal($("#operatorModal"));
+			operatorModal.show();
+		},
+		error: function () {
+			console.log("Error loading the PHP view for the operator call dialog.");
+		},
+	});
+}
+
+function closeOperatorDialog() {
+	var operatorModal = bootstrap.Modal.getInstance($("#operatorModal"));
+	if (operatorModal) {
+		operatorModal.hide();
+		$("#operatorModal").remove();
+	}
+}
+
+function saveOperator() {
+	var operatorInput = $("#operator_callsign");
+	var operatorCallsign = operatorInput.val();
+
+	if (operatorCallsign != "" && operatorCallsign != sc_account_call) {
+		$.ajax({
+			url: base_url + "index.php/operator/saveOperator",
+			method: "POST",
+			type: "post",
+			data: {
+				operator_callsign: operatorCallsign,
+			},
+		});
+		closeOperatorDialog();
+		// console.log("saveOperator executed");
+		// console.log("operator:" + operatorCallsign);
+	} else {
+		operatorInput.addClass("is-invalid");
+	}
+}
