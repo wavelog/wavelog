9489ae1e582fe7e9c464944c90bcbb28e0ed0f9b
HB9HIL <fabian.berg@hb9hil.org>
2024-07-11 14:40:10 +0200

    make sure the user does not use the default encryption key

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -55,7 +55,7 @@
                             <small><a class="" href="<?php echo site_url('user/forgot_password'); ?>"><?= __("Forgot your password?"); ?></a></small>
                         </div>
                         <div class="col text-end">
-                            <?php if ($https_check == true) { ?> <!-- we only want to create these cookies if the site is reached by https -->
+                            <?php if ($https_check == true && $this->config->item('encryption_key') != 'flossie1234555541') { ?> <!-- we only want to create these cookies if the site is reached by https -->
                                 <input type="checkbox" value="1" name="keep_login" id="keep_login" />
                                 <label for="keep_login"><small><?= __("Keep me logged in"); ?></small></label>
                             <?php } ?>
