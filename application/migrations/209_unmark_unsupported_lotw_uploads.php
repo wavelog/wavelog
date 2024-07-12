d3d31f3bdb41df357bb2644831a7a7ff157be94b
phl0 <github@florian-wolters.de>
2024-07-12 10:28:48 +0200

    Set LoTW sent status to I (ignore)

--- application/migrations/209_unmark_unsupported_lotw_uploads.php
+++ application/migrations/209_unmark_unsupported_lotw_uploads.php
@@ -6,7 +6,7 @@ class Migration_unmark_unsupported_lotw_uploads extends CI_Migration
    public function up() {
 
       $this->db->where_in('COL_PROP_MODE', $this->config->item('lotw_unsupported_prop_modes'));
-      $this->db->update($this->config->item('table_name'), array('COL_LOTW_QSLSDATE' => null, 'COL_LOTW_QSL_SENT' => 'N'));
+      $this->db->update($this->config->item('table_name'), array('COL_LOTW_QSLSDATE' => null, 'COL_LOTW_QSL_SENT' => 'I'));
    }
 
    public function down() {
