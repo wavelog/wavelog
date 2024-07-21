7d4cb8f6e97c25c11a1b9df3bbe9c21b2dacc429
HB9HIL <fabian.berg@hb9hil.org>
2024-07-21 19:51:03 +0200

    wait until all migrations are done

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -1,14 +1,18 @@
-<?php class Migrate extends CI_Controller {
+<?php
+class Migrate extends CI_Controller {
 
   public function index() {
     $this->load->library('Migration');
 
     if (!$this->migration->latest()) {
       show_error($this->migration->error_string());
-      echo 'error';
+      $result = 'error';
     } else {
-      echo 'success';
+      while (file_exists(APPPATH . 'cache/.migration_running')) {
+        sleep(1);
+      }
+      $result = 'success';
     }
-
+    echo $result;
   }
 }
