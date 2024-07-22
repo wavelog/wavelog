ba1b1827c518ba524e4e6ca140dd2e5ce738ffec
Andreas Kristiansen <6977712+AndreasK79@users.noreply.github.com>
2024-07-22 19:40:42 +0200

    Corrected which elevation to print

--- application/views/satellite/passtable.php
+++ application/views/satellite/passtable.php
@@ -13,13 +13,13 @@ if ($filtered) {
 					<th>LOS El</th>
 				</tr>';
 			foreach ($filtered as $pass) {
-				echo '<tr align="center" class="mode2">';
+				echo '<tr>';
 				echo '<td>' . $pass->satname . '</td>';
 				echo '<td>' . Predict_Time::daynum2readable($pass->visible_aos, $zone, $format) . '</td>';
 				echo '<td>' . returntimediff(Predict_Time::daynum2readable($pass->visible_aos, $zone, $format), Predict_Time::daynum2readable($pass->visible_los, $zone, $format)) . '</td>';
 				echo '<td>' . round($pass->visible_aos_az) . ' (' . azDegreesToDirection($pass->visible_aos_az) . ')</td>';
 				echo '<td>' . round($pass->visible_aos_el) . '</td>';
-				echo '<td>' . round($pass->visible_max_el) . '</td>';
+				echo '<td>' . round($pass->max_el) . '</td>';
 				echo '<td>' . Predict_Time::daynum2readable($pass->visible_los, $zone, $format) . '</td>';
 				echo '<td>' . round($pass->visible_los_az) . ' (' . azDegreesToDirection($pass->visible_los_az) . ')</td>';
 				echo '<td>' . round($pass->visible_los_el) . '</td>';
