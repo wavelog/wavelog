890b300af72dc21e843cb2fab4686b8c0eedb4d0
phl0 <github@florian-wolters.de>
2024-08-01 11:44:15 +0200

    Look for QSOs with empty AZ as well

--- application/models/Logbook_model.php
+++ application/models/Logbook_model.php
@@ -4678,7 +4678,7 @@ function lotw_last_qsl_date($user_id) {
     public function update_distances(){
         $this->db->select("COL_PRIMARY_KEY, COL_GRIDSQUARE, station_gridsquare");
         $this->db->join('station_profile', 'station_profile.station_id = '.$this->config->item('table_name').'.station_id');
-        $this->db->where("((COL_DISTANCE is NULL) or (COL_DISTANCE = 0))");
+        $this->db->where("((COL_DISTANCE is NULL) or (COL_DISTANCE = 0) or (COL_ANT_AZ is NULL))");
         $this->db->where("COL_GRIDSQUARE is NOT NULL");
         $this->db->where("COL_GRIDSQUARE != ''");
         $this->db->where("COL_GRIDSQUARE != station_gridsquare");
