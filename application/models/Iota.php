4f207dca1d7e4a3edc3e61c8ee88e1ff0b6dd185
Andreas Kristiansen <6977712+AndreasK79@users.noreply.github.com>
2024-08-16 08:03:27 +0200

    Fixed another one

--- application/models/Iota.php
+++ application/models/Iota.php
@@ -196,6 +196,7 @@ class IOTA extends CI_Model {
 		if ($postdata['mode'] != 'All') {
 			$sql .= " and (col_mode = ? or col_submode = ?)";
 			$binding[] = $postdata['mode'];
+			$binding[] = $postdata['mode'];
 		}
 
 		if ($postdata['includedeleted'] == NULL) {
