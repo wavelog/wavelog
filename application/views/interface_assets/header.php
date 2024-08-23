0ad36660e28c7e79fbcf8d2e63a00aac1a36c767
DB4SCW <dev@db4scw.de>
2024-08-23 13:17:14 +0200

    change menu text in main layout

--- application/views/interface_assets/header.php
+++ application/views/interface_assets/header.php
@@ -393,7 +393,7 @@
 
 										<li><a class="dropdown-item" href="<?php echo site_url('cabrillo'); ?>" title="Cabrillo Export"><i class="fas fa-sync"></i> <?= __("Cabrillo Export"); ?></a></li>
 
-										<li><a class="dropdown-item" href="<?php echo site_url('reg1test'); ?>" title="REG1TEST EDI Export"><i class="fas fa-sync"></i> <?= __("REG1TEST EDI Export"); ?></a></li>
+										<li><a class="dropdown-item" href="<?php echo site_url('reg1test'); ?>" title="EDI Export"><i class="fas fa-sync"></i> <?= __("EDI Export"); ?></a></li>
 
 										<li><a class="dropdown-item" href="<?php echo site_url('cfdexport'); ?>" title="CFD Export"><i class="fas fa-sync"></i> <?= __("CFD Export"); ?></a></li>
 									</ul>
