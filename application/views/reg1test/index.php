463181c6c77e1fed1d23a02964ec0d201b8df598
DB4SCW <dev@db4scw.de>
2024-08-26 13:49:25 +0000

    fixed a few element ids in the frontend

--- application/views/reg1test/index.php
+++ application/views/reg1test/index.php
@@ -69,12 +69,12 @@
 				<div hidden="true" class="mb-3 d-flex align-items-center row additionalinfo">
 					<div class="col-md-4 control-label" for="operators"><?= __("Operators") ?> </div>
 					<input class="form-control my-1 me-sm-2 col-md-6 w-25 w-lg-75" maxlength="75" id="operators" type="operators" name="operators" aria-label="operators">
-					<small id="club_hint" class="form-text text-muted col-md-4"><?= __("List of all operators. Seperated with a semicolon ';'. The responsible operator is not needed here."); ?></small>
+					<small id="operators_hint" class="form-text text-muted col-md-4"><?= __("List of all operators. Seperated with a semicolon ';'. The responsible operator is not needed here."); ?></small>
 				</div>
 				<div hidden="true" class="mb-3 d-flex align-items-center row additionalinfo">
 					<div class="col-md-4 control-label" for="contestaddress1"><?= __("Contest Address 1") ?> </div>
 					<input class="form-control my-1 me-sm-2 col-md-6 w-25 w-lg-75" maxlength="75" id="contestaddress1" type="text" name="contestaddress1" aria-label="contestaddress1">
-					<small id="club_hint" class="form-text text-muted col-md-4"><?= __("Address of the QTH used during the contest."); ?></small>
+					<small id="contestaddress1_hint" class="form-text text-muted col-md-4"><?= __("Address of the QTH used during the contest."); ?></small>
 				</div>
 				<div hidden="true" class="mb-3 d-flex align-items-center row additionalinfo">
 					<div class="col-md-4 control-label" for="contestaddress2"><?= __("Contest Address 2") ?> </div>
@@ -83,7 +83,7 @@
 				<div hidden="true" class="mb-3 d-flex align-items-center row additionalinfo">
 					<div class="col-md-4 control-label" for="address1"><?= __("Operator Address 1") ?> </div>
 					<input class="form-control my-1 me-sm-2 col-md-6 w-25 w-lg-75" maxlength="75" id="address1" type="text" name="address1" aria-label="address1">
-					<small id="club_hint" class="form-text text-muted col-md-4"><?= __("Address of the responsible operator."); ?></small>
+					<small id="address1_hint" class="form-text text-muted col-md-4"><?= __("Address of the responsible operator."); ?></small>
 				</div>
 				<div hidden="true" class="mb-3 d-flex align-items-center row additionalinfo">
 					<div class="col-md-4 control-label" for="address2"><?= __("Operator Address 2") ?> </div>
@@ -108,32 +108,32 @@
 				<div hidden="true" class="mb-3 d-flex align-items-center row additionalinfo">
 					<div class="col-md-4 control-label" for="txequipment"><?= __("Transmit Equipment Description") ?> </div>
 					<input class="form-control my-1 me-sm-2 col-md-6 w-25 w-lg-75" maxlength="75" id="txequipment" type="text" name="txequipment" aria-label="txequipment">
-					<small id="club_hint" class="form-text text-muted col-md-4"><?= __("Short description of the used equipment."); ?></small>
+					<small id="txequipment_hint" class="form-text text-muted col-md-4"><?= __("Short description of the used equipment."); ?></small>
 				</div>
 				<div hidden="true" class="mb-3 d-flex align-items-center row additionalinfo">
 					<div class="col-md-4 control-label" for="power"><?= __("Transmit Power (W)") ?> </div>
 					<input class="form-control my-1 me-sm-2 col-md-6 w-25 w-lg-75" maxlength="75" id="power" type="text" name="power" aria-label="power">
-					<small id="club_hint" class="form-text text-muted col-md-4"><?= __("TX Power during the contest in Watt."); ?></small>
+					<small id="power_hint" class="form-text text-muted col-md-4"><?= __("TX Power during the contest in Watt."); ?></small>
 				</div>
 				<div hidden="true" class="mb-3 d-flex align-items-center row additionalinfo">
 					<div class="col-md-4 control-label" for="rxequipment"><?= __("Receive Equipment Description") ?> </div>
 					<input class="form-control my-1 me-sm-2 col-md-6 w-25 w-lg-75" maxlength="75" id="rxequipment" type="text" name="rxequipment" aria-label="rxequipment">
-					<small id="club_hint" class="form-text text-muted col-md-4"><?= __("If you used another gear for RX, then describe it here."); ?></small>
+					<small id="rxequipment_hint" class="form-text text-muted col-md-4"><?= __("If you used another gear for RX, then describe it here."); ?></small>
 				</div>
 				<div hidden="true" class="mb-3 d-flex align-items-center row additionalinfo">
 					<div class="col-md-4 control-label" for="antenna"><?= __("Antenna Description") ?> </div>
 					<input class="form-control my-1 me-sm-2 col-md-6 w-25 w-lg-75" maxlength="75" id="antenna" type="text" name="antenna" aria-label="antenna">
-					<small id="club_hint" class="form-text text-muted col-md-4"><?= __("What kind of antenna was used."); ?></small>
+					<small id="antenna_hint" class="form-text text-muted col-md-4"><?= __("What kind of antenna was used."); ?></small>
 				</div>
 				<div hidden="true" class="mb-3 d-flex align-items-center row additionalinfo">
 					<div class="col-md-4 control-label" for="antennaheight"><?= __("Antenna Height Above Ground (m)") ?> </div>
 					<input class="form-control my-1 me-sm-2 col-md-6 w-25 w-lg-75" maxlength="75" id="antennaheight" type="text" name="antennaheight" aria-label="antennaheight">
-					<small id="club_hint" class="form-text text-muted col-md-4"><?= __("Height of the antenna above the ground."); ?></small>
+					<small id="antennaheight_hint" class="form-text text-muted col-md-4"><?= __("Height of the antenna above the ground."); ?></small>
 				</div>
 				<div hidden="true" class="mb-3 d-flex align-items-center row additionalinfo">
 					<div class="col-md-4 control-label" for="soapbox"><?= __("Soapbox") ?> </div>
 					<input class="form-control my-1 me-sm-2 col-md-6 w-25 w-lg-75" id="soapbox" type="text" name="soapbox" aria-label="soapbox">
-					<small id="club_hint" class="form-text text-muted col-md-4"><?= __("Any other remarks."); ?></small>
+					<small id="soapbox_hint" class="form-text text-muted col-md-4"><?= __("Any other remarks."); ?></small>
 				</div>
 				<div hidden="true" class="mb-3 d-flex align-items-center row additionalinfo">
 					<div class="col-md-4 control-label" for="button1id"></div>
