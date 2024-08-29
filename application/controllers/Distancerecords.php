6be5ce0f726e3338b54be5bc3dd9566accc3c8e9
phl0 <github@florian-wolters.de>
2024-08-29 17:14:45 +0200

    Add page with distance records per SAT

---
+++ application/controllers/Distancerecords.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Distancerecords extends CI_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('user_model');
+        if(!$this->user_model->authorize(2)) { $this->session->set_flashdata('error', __("You're not allowed to do that!")); redirect('dashboard'); }
+    }
+
+    public function index()
+    {
+
+        $this->load->model('distancerecords_model');
+
+        // Render Page
+        $data['page_title'] = __("Distance Records");
+        $data['distances'] = $this->distancerecords_model->get_records();
+
+        $this->load->view('interface_assets/header', $data);
+        $this->load->view('distancerecords/index');
+        $this->load->view('interface_assets/footer');
+    }
+
+    public function distancerecords() {
+    }
+
+}
---
+++ application/views/distancerecords/index.php
@@ -0,0 +1,53 @@
+<div class="container">
+	<br>
+		<h2><?php echo $page_title; ?></h2>
+	<p><?php echo __("This page lists distance records per satellite based on gridsquares."); ?>
+	<?php
+		if ($distances) {
+			if($this->session->userdata('user_date_format')) {
+				// If Logged in and session exists
+				$custom_date_format = $this->session->userdata('user_date_format');
+			} else {
+				// Get Default date format from /config/wavelog.php
+				$custom_date_format = $this->config->item('qso_date_format');
+			}
+	?>
+	
+	<table style="width: 100%" id="distrectable" class="distrectable table table-sm table-striped table-hover">
+	<thead>
+		
+	<tr>
+		<th style="text-align: center"><?= __("Satellite") ?></th>
+		<th style="text-align: center"><?= __("Distance") ?></th>
+		<th style="text-align: center"><?= __("Date") ?></th>
+		<th style="text-align: center"><?= __("Time") ?></th>
+		<th style="text-align: center"><?= __("Callsign") ?></th>
+		<th style="text-align: center"><?= __("Gridsquare") ?></th>
+	</tr>
+	</thead>
+	
+	<tbody>
+	<?php
+		if ($distances->num_rows() > 0) {
+			foreach ($distances->result() as $row) {
+	?>
+	
+	<tr>
+		<td style="text-align: center"><?php echo $row->sat; ?></td>
+		<td style="text-align: center"><?php printf("%.01f", floatval($row->distance)); ?> km</td>
+		<td style="text-align: center"><?php $timestamp = strtotime($row->time); echo date($custom_date_format, $timestamp); ?></td>
+		<td style="text-align: center"><?php $timestamp = strtotime($row->time); echo date('H:i', $timestamp); ?></td>
+		<td style="text-align: center"><?php echo $row->callsign; ?></td>
+		<td style="text-align: center"><?php echo $row->grid; ?></td>
+	</tr>
+	<?php
+		  }
+		}
+	?>
+	
+	</tbody>
+	</table>
+	<?php } else {
+        echo '<div class="alert alert-danger" role="alert">' . __("Nothing found!") . '</div>';
+    }?>
+</div>
