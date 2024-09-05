16681878160c0e3e2c22454a67ed504d2167ed9b
karuru <karuru@aerodefense.co.uk>
2024-09-05 10:29:59 +0800

    fix lotw upload: unnecessary column

--- application/views/lotw_views/adif_views/adif_export.php
+++ application/views/lotw_views/adif_views/adif_export.php
@@ -48,12 +48,12 @@ $cert2 = str_replace("-----END CERTIFICATE-----", "", $cert1);
 
 <?php if($qso->COL_FREQ_RX != "" && $qso->COL_FREQ_RX != "0") { $freq_in_mhz_rx = $qso->COL_FREQ_RX / 1000000; ?><FREQ_RX:<?php echo strlen($freq_in_mhz_rx); ?>><?php echo $freq_in_mhz_rx; } ?>
 
-<?php if(isset($qso->COL_PROP_MODE)) { ?><PROP_MODE:<?php echo strlen($qso->COL_PROP_MODE); ?>><?php echo strtoupper($qso->COL_PROP_MODE); } ?>
+<?php if($qso->COL_PROP_MODE) { ?><PROP_MODE:<?php echo strlen($qso->COL_PROP_MODE); ?>><?php echo strtoupper($qso->COL_PROP_MODE); } ?>
 
-<?php if(isset($qso->COL_SAT_NAME)) { $satellite_name_check = $CI->lotw_satellite_map(strtoupper($qso->COL_SAT_NAME)); if($satellite_name_check != FALSE) { $satname = $satellite_name_check; } else { $satname = $qso->COL_SAT_NAME; } ?>
+<?php if($qso->COL_SAT_NAME) { $satellite_name_check = $CI->lotw_satellite_map(strtoupper($qso->COL_SAT_NAME)); if($satellite_name_check != FALSE) { $satname = $satellite_name_check; } else { $satname = $qso->COL_SAT_NAME; } ?>
 <SAT_NAME:<?php echo strlen($satname); ?>><?php echo strtoupper($satname); } ?>
 
-<?php if(isset($qso->COL_BAND_RX)) { ?><BAND_RX:<?php echo strlen($qso->COL_BAND_RX); ?>><?php echo strtoupper($qso->COL_BAND_RX); } ?>
+<?php if($qso->COL_BAND_RX) { ?><BAND_RX:<?php echo strlen($qso->COL_BAND_RX); ?>><?php echo strtoupper($qso->COL_BAND_RX); } ?>
 
 <?php $date_on = strtotime($qso->COL_TIME_ON); $new_date = date('Y-m-d', $date_on); ?>
 <QSO_DATE:<?php echo strlen($new_date); ?>><?php echo $new_date; ?>
@@ -163,4 +163,3 @@ if(isset($qso->COL_SAT_NAME)) {
 <EOR>
 
 <?php } ?>
-
