202f44dce7e8ce746894805c32d717c28a6a03af
phl0 <github@florian-wolters.de>
2024-10-01 16:58:48 +0200

    Show last sent date in case of modified for qrz

--- src/QSLManager/QSO.php
+++ src/QSLManager/QSO.php
@@ -477,10 +477,10 @@ class QSO
 
 			if ($data['COL_QRZCOM_QSO_UPLOAD_DATE'] != null) {
 				$timestamp = strtotime($data['COL_QRZCOM_QSO_UPLOAD_DATE']);
-				$qrzstring .=  " ".($timestamp!=''?date($custom_date_format, $timestamp):'');
+				$qrzstring .=  "<br />(".__("last sent")." ".($timestamp!=''?date($custom_date_format, $timestamp):'').")";
 			}
 
-			$qrzstring .= "\" data-bs-toggle=\"tooltip\"";
+			$qrzstring .= "\" data-bs-toggle=\"tooltip\" data-bs-html=\"true\"";
 		}
 
 		if ($data['COL_QRZCOM_QSO_UPLOAD_STATUS'] == "I") {
