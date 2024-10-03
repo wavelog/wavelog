1db967b6d51cfc67a07976ef4be6f7516362bb7c
HB9HIL <fabian.berg@hb9hil.org>
2024-10-03 13:14:18 +0000

    some more details in error message

--- application/models/Logbook_model.php
+++ application/models/Logbook_model.php
@@ -3560,7 +3560,7 @@ class Logbook_model extends CI_Model {
 		$time_on = date('Y-m-d', strtotime($record['qso_date'] ?? '1970-01-01')) . " " . date('H:i:s', strtotime($record['time_on'] ?? '00:00:00'));
 
 		if (($record['call'] ?? '') == '') {
-			log_message("Error","Trying to import QSO without Call for station_id ".$station_id);
+			log_message("Error", "Trying to import QSO without Call for station_id " . $station_id . ". QSO Date/Time: " . $time_on . " Mode: " . ($record['mode'] ?? '') . " Band: " . ($record['band'] ?? ''));
 			$returner['error']=__("QSO on")." ".$time_on.": ".__("You tried to import a QSO without any given CALL. This QSO wasn't imported. It's invalid");
 			return($returner);
 		}
