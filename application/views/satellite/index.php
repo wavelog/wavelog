30c6124565bc2376f9d75e3faad42e508ab10b8c
phl0 <github@florian-wolters.de>
2024-10-06 09:24:36 +0200

    Do not error out on empty exportname

--- application/views/satellite/index.php
+++ application/views/satellite/index.php
@@ -30,7 +30,7 @@
 				<?php foreach ($satellites as $sat) { ?>
 				<tr>
 					<td style="text-align: center; vertical-align: middle;" class="satellite_<?php echo $sat->id ?>"><?php echo htmlentities($sat->satname) ?></td>
-					<td style="text-align: center; vertical-align: middle;"><?php echo htmlentities($sat->exportname) ?></td>
+					<td style="text-align: center; vertical-align: middle;"><?php echo $sat->exportname ? htmlentities($sat->exportname) : '' ?></td>
 					<?php echo '<td style="text-align: center; vertical-align: middle;"><span class="badge ';
 					switch (strtoupper($sat->orbit)) {
 					case 'LEO':
