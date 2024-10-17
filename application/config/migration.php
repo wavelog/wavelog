2c29a60ac2da57ec823a68614b204b5edbec00a7
phl0 <github@florian-wolters.de>
2024-10-17 14:34:18 +0200

    Increase migration

--- application/config/migration.php
+++ application/config/migration.php
@@ -22,7 +22,7 @@ $config['migration_enabled'] = TRUE;
 |
 */
 
-$config['migration_version'] = 222;
+$config['migration_version'] = 223;
 
 /*
 |--------------------------------------------------------------------------
