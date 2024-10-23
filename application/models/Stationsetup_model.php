ababdc0c685b76ceff960a21ced68245cb581f2a
HB9HIL <fabian.berg@hb9hil.org>
2024-10-23 23:25:10 +0200

    allow request without session

--- application/models/Stationsetup_model.php
+++ application/models/Stationsetup_model.php
@@ -2,11 +2,13 @@
 
 class Stationsetup_model extends CI_Model {
 
-	function getContainer($id) {
+	function getContainer($id, $session = true) {
 		// Clean ID
 		$clean_id = $this->security->xss_clean($id);
 
-		$this->db->where('user_id', $this->session->userdata('user_id'));
+		if ($session) {
+			$this->db->where('user_id', $this->session->userdata('user_id'));
+		} 
 		$this->db->where('logbook_id', $clean_id);
 		return $this->db->get('station_logbooks');
 	}
