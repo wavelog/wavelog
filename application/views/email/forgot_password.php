38ad6df1fea3f8eb33bd962d38aa8fa81545e177
HB9HIL <fabian.berg@hb9hil.org>
2024-10-28 13:26:51 +0100

    i don't see the point in showing the reset code like that. it's already in the URL and the user has to use the link

--- application/views/email/forgot_password.php
+++ application/views/email/forgot_password.php
@@ -6,7 +6,7 @@ $message['body'] = __("Hi,
 
 You or someone else has requested a password reset on your Wavelog account.") . "\n\n" .
 
-sprintf(__("Your password reset code is: %s"), $reset_code) . "\n\n" .
+// sprintf(__("Your password reset code is: %s"), $reset_code) . "\n\n" .
 
 sprintf(__("Click here to reset your password: %s"), site_url('user/reset_password/') . $reset_code) . "\n\n" .
 
