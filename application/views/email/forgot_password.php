98f153259af52093597151cc35a99c25a44c7291
HB9HIL <fabian.berg@hb9hil.org>
2024-10-28 11:03:20 +0100

    forgot password email

--- application/views/email/forgot_password.php
+++ application/views/email/forgot_password.php
@@ -1,15 +1,16 @@
 <?= 
+html_entity_decode(
+    __("Hi,
 
-__("Hi,
+    You or someone else has requested a password reset on your Wavelog account.") . "\n\n" . __("Log View") . "\n\n" .
 
-You or someone else has requested a password reset on your Wavelog account.");
+    sprintf(__("Your password reset code is: %s"), $reset_code) . "\n\n" .
 
-sprintf(__("Your password reset code is: %s"), $reset_code);
+    sprintf(__("Click here to reset your password: %s"), site_url('user/reset_password/') . $reset_code) . "\n\n" .
 
-sprintf(__("Click here to reset your password: %s"), site_url('user/reset_password/') . $reset_code);
+    __("If you didn't request this just ignore.
 
-__("If you didn't request this just ignore.
+    Regards,
 
-Regards,
-
-Wavelog");
+    Wavelog")
+);
