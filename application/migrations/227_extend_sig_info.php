657a50b4ec1b10a33b20643d03c8c816dc2100bb
int2001 <joerg@dj7nt.de>
2024-11-11 10:29:36 +0000

    Hamronize SIG Col at log as well

--- application/migrations/227_extend_sig_info.php
+++ application/migrations/227_extend_sig_info.php
@@ -4,6 +4,7 @@ class Migration_extend_sig_info  extends CI_Migration
 {
 
 	public function up() {
+		$this->db->query("ALTER TABLE ".$this->config->item('table_name')." CHANGE COLUMN COL_SIG COL_SIG VARCHAR(64) NULL DEFAULT NULL");
 		$this->db->query("ALTER TABLE ".$this->config->item('table_name')." CHANGE COLUMN COL_SIG_INFO COL_SIG_INFO VARCHAR(128) NULL DEFAULT NULL");
 		$this->db->query("ALTER TABLE ".$this->config->item('table_name')." CHANGE COLUMN COL_MY_SIG_INFO COL_MY_SIG_INFO VARCHAR(128) NULL DEFAULT NULL");
 		$this->db->query("ALTER TABLE ".$this->config->item('table_name')." CHANGE COLUMN COL_MY_SIG COL_MY_SIG VARCHAR(64) NULL DEFAULT NULL");
