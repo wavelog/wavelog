b92c561425b13a981a857dde836c660611b4e19c
HB9HIL <fabian.berg@hb9hil.org>
2024-11-13 09:46:11 +0100

    fix handling if no enter was pressed

--- assets/js/sections/qrg_handler.js
+++ assets/js/sections/qrg_handler.js
@@ -52,64 +52,73 @@ async function set_qrg() {
 	}
 }
 
+function set_new_qrg() {
+	let new_qrg = $('#freq_calculated').val().trim();
+	let parsed_qrg = parseFloat(new_qrg);
+	let unit = $('#qrg_unit').html();
+
+	// check if the input contains a unit and parse the qrg
+	if (/^\d+(\.\d+)?\s*hz$/i.test(new_qrg)) {
+		unit = 'Hz';
+		parsed_qrg = parseFloat(new_qrg);
+	} else if (/^\d+(\.\d+)?\s*khz$/i.test(new_qrg)) {
+		unit = 'kHz';
+		parsed_qrg = parseFloat(new_qrg);
+	} else if (/^\d+(\.\d+)?\s*mhz$/i.test(new_qrg)) {
+		unit = 'MHz';
+		parsed_qrg = parseFloat(new_qrg);
+	} else if (/^\d+(\.\d+)?\s*ghz$/i.test(new_qrg)) {
+		unit = 'GHz';
+		parsed_qrg = parseFloat(new_qrg);
+	}
+
+	// update the unit if there was any change
+	$('#qrg_unit').html(unit);
+
+	// calculate the other stuff
+	let qrg_hz;
+	switch (unit) {
+		case 'Hz':
+			qrg_hz = parsed_qrg;
+			localStorage.setItem('qrgunit_' + $('#band').val(), 'Hz');
+			break;
+		case 'kHz':
+			qrg_hz = parsed_qrg * 1000;
+			localStorage.setItem('qrgunit_' + $('#band').val(), 'kHz');
+			break;
+		case 'MHz':
+			qrg_hz = parsed_qrg * 1000000;
+			localStorage.setItem('qrgunit_' + $('#band').val(), 'MHz');
+			break;
+		case 'GHz':
+			qrg_hz = parsed_qrg * 1000000000;
+			localStorage.setItem('qrgunit_' + $('#band').val(), 'GHz');
+			break;
+		default:
+			qrg_hz = 0;
+			console.error('Invalid unit');
+	}
+
+	$('#frequency').val(qrg_hz);
+	$('#freq_calculated').val(parsed_qrg);
+	$('#band').val(frequencyToBand(qrg_hz));
+
+}
+
 $('#frequency').on('change', function () {
+	console.log('frequency changed');
 	set_qrg();
 });
 
+$('#freq_calculated').on('change', function () {
+	console.log('freq_calculated changed');
+	set_new_qrg();
+});
+
 $('#freq_calculated').on('keydown', function (e) {
-    if (e.which === 13) {
-        e.preventDefault();
-
-        let new_qrg = $('#freq_calculated').val().trim();
-        let parsed_qrg = parseFloat(new_qrg);
-        let unit = $('#qrg_unit').html();
-
-        // check if the input contains a unit and parse the qrg
-        if (/^\d+(\.\d+)?\s*hz$/i.test(new_qrg)) {
-            unit = 'Hz';
-            parsed_qrg = parseFloat(new_qrg);
-        } else if (/^\d+(\.\d+)?\s*khz$/i.test(new_qrg)) {
-            unit = 'kHz';
-            parsed_qrg = parseFloat(new_qrg);
-        } else if (/^\d+(\.\d+)?\s*mhz$/i.test(new_qrg)) {
-            unit = 'MHz';
-            parsed_qrg = parseFloat(new_qrg);
-        } else if (/^\d+(\.\d+)?\s*ghz$/i.test(new_qrg)) {
-            unit = 'GHz';
-            parsed_qrg = parseFloat(new_qrg);
-        }
-
-        // update the unit if there was any change
-        $('#qrg_unit').html(unit);
-
-        // calculate the other stuff
-        let qrg_hz;
-        switch (unit) {
-            case 'Hz':
-                qrg_hz = parsed_qrg;
-				localStorage.setItem('qrgunit_' + $('#band').val(), 'Hz');
-                break;
-            case 'kHz':
-                qrg_hz = parsed_qrg * 1000;
-				localStorage.setItem('qrgunit_' + $('#band').val(), 'kHz');
-                break;
-            case 'MHz':
-                qrg_hz = parsed_qrg * 1000000;
-				localStorage.setItem('qrgunit_' + $('#band').val(), 'MHz');
-                break;
-            case 'GHz':
-                qrg_hz = parsed_qrg * 1000000000;
-				localStorage.setItem('qrgunit_' + $('#band').val(), 'GHz');
-                break;
-            default:
-                qrg_hz = 0;
-                console.error('Invalid unit');
-        }
-
-        $('#frequency').val(qrg_hz);
-        $('#freq_calculated').val(parsed_qrg);
-        $('#band').val(frequencyToBand(qrg_hz));
-
+	if (e.which === 13) {
+		e.preventDefault();
+		set_new_qrg();      
 		$("#callsign").trigger("focus");
     }
 });
