1ebc8664224de6c6ac564de38274f9d1d443980b
Joerg (DJ7NT) <int2001@users.noreply.github.com>
2024-11-20 11:34:11 +0100

    Update application/views/view_log/partial/log_ajax.php
    tnx
    
    Co-authored-by: Fabian Berg <fabian.berg@hb9hil.org>

--- application/views/view_log/partial/log_ajax.php
+++ application/views/view_log/partial/log_ajax.php
@@ -297,7 +297,7 @@ function echo_table_col($row, $name) {
                         <span <?php 
 				if ($row->COL_CLUBLOG_QSO_UPLOAD_STATUS == "Y") { 
 					echo 'title="'.__("Sent").($row->COL_CLUBLOG_QSO_UPLOAD_DATE != null ? " ".date($custom_date_format, strtotime($row->COL_CLUBLOG_QSO_UPLOAD_DATE)) : '').'" data-bs-toggle="tooltip"'; 
-				} elseif ($row->COL_CLUBLOG_QSO_UPLOAD_STATUS == 'M' && $row->COL_CLUBLOG_QSO_UPLOAD_STATUS != NULL) { 
+				} elseif ($row->COL_CLUBLOG_QSO_UPLOAD_STATUS == 'M' && $row->COL_CLUBLOG_QSO_UPLOAD_DATE != NULL) { 
 					echo 'title="'.__("Modified")."<br />(".__("last sent")." ".date($custom_date_format, strtotime($row->COL_CLUBLOG_QSO_UPLOAD_DATE)).")".'" data-bs-toggle="tooltip" data-bs-html="true"'; 
 				} elseif ($row->COL_CLUBLOG_QSO_UPLOAD_STATUS == 'I') { 
 					echo 'title="'.__("Invalid (Ignore)").'" data-bs-toggle="tooltip"'; 
