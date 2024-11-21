78ccb70fc7a0a3c1a46f7bfb0a144b6ec2edfb13
phl0 <github@florian-wolters.de>
2024-11-21 15:06:35 +0100

    Correct wording

--- application/views/qso/index.php
+++ application/views/qso/index.php
@@ -546,9 +546,9 @@
             <?php } ?>
 
             <div class="mb-3">
-              <label for="email"><?= __("eMail"); ?></label>
+              <label for="email"><?= __("E-mail"); ?></label>
               <input class="form-control" id="email" type="text" name="email" value="" />
-              <small id="MailHelp" class="form-text text-muted"><?= __("eMail-Adress of QSO-Partner"); ?></small>
+              <small id="MailHelp" class="form-text text-muted"><?= __("E-mail address of QSO-Partner"); ?></small>
             </div>
 
 
