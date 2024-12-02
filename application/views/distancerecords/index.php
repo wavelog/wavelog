12958e84eced26283e0f5d445f1acfc3b4ae867c
phl0 <github@florian-wolters.de>
2024-12-02 17:12:13 +0100

    Add displayname to distance records table

--- application/views/distancerecords/index.php
+++ application/views/distancerecords/index.php
@@ -31,7 +31,7 @@
 
 	<tr>
 		<td style="text-align: center"><?php echo $i; ?></td>
-		<td style="text-align: center"><a href="javascript:displayDistanceQsos('<?php echo $row->sat; ?>')"><?php echo $row->sat; ?></a></td>
+		<td style="text-align: center"><a href="javascript:displayDistanceQsos('<?php echo $row->sat; ?>')"><?php echo ($row->sat_displayname != null ? $row->sat_displayname." (".$row->sat.")" : $row->sat); ?></a></td>
 		<td style="text-align: right"><?php printf("%.01f", (floatval($row->distance) * $factor)); echo ' '.$unit; ?></td>
 		<td style="text-align: center"><?php $timestamp = strtotime($row->time ?? ''); echo date($custom_date_format, $timestamp); ?></td>
 		<td style="text-align: center"><?php $timestamp = strtotime($row->time ?? ''); echo date('H:i', $timestamp); ?></td>
