470af78a89073826bb5d6c93c5e27d80d5fe00e3
phl0 <github@florian-wolters.de>
2024-12-02 15:17:52 +0100

    Fix country in sat records

--- application/models/Distancerecords_model.php
+++ application/models/Distancerecords_model.php
@@ -89,6 +89,7 @@ class Distancerecords_model extends CI_Model {
 		$this->load->model('logbooks_model');
 		$logbooks_locations_array = $this->logbooks_model->list_logbook_relationships($this->session->userdata('active_station_logbook'));
 		$this->db->join('station_profile', 'station_profile.station_id = '.$this->config->item('table_name').'.station_id');
+		$this->db->join('dxcc_entities', $this->config->item('table_name') . '.col_dxcc = dxcc_entities.adif', 'left outer');
 		$this->db->where('COL_SAT_NAME', $sat);
 		$this->db->where_in($this->config->item('table_name').'.station_id', $logbooks_locations_array);
 		$this->db->order_by("COL_DISTANCE", "desc");
