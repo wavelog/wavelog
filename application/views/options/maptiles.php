1fc6a157bf974b72731080c2faa64853dbb79a83
HB9HIL <fabian.berg@hb9hil.org>
2024-12-06 16:33:06 +0100

    feat[maptile options]: UI Adjustments and Link to Wiki for tested Settings

--- application/views/options/maptiles.php
+++ application/views/options/maptiles.php
@@ -43,33 +43,37 @@
 
                     <?php echo form_open('options/maptiles_save'); ?>
 
-                        <p class="alert alert-info">
-                            <?= __("This modifies the map source in various locations within Wavelog. Do not change any values here unless you are confident in what you are doing."); ?>
-                        </p>
+                        <?php if (!$this->session->flashdata('warning')) { ?>
+                            <p class="alert alert-danger">
+                                <b><u><?= __("Important"); ?></u></b><br>
+                                <?= sprintf(__("This modifies the map source in various locations within Wavelog. Do not change any values here unless you are confident in what you are doing. Test each change on your %sDashboard%s!"), '<a href="' . site_url('/') . '" target="_blank"><u>', '</u></a>'); ?><br>
+                                <?= sprintf(__("You can find a list of tested settings and all default values in the %sWavelog Wiki%s."), '<a href="https://github.com/wavelog/wavelog/wiki/Maptile-Server" target="_blank"><u>', '</u></a>'); ?>
+                            </p>
+                        <?php } ?>
                         <div class="mb-3">
                             <label for="maptile_server_url"><?= __("Maptiles Server URL"); ?></label>
                             <input type="text" name="maptile_server_url" class="form-control" id="maptile_server_url" aria-describedby="maptile_server_urlHelp" value="<?php echo $maptile_server_url; ?>">
-                            <small id="maptile_server_urlHelp" class="form-text text-muted"><?= __("URL of the map server which serves the maptiles."); ?> <?= sprintf(__("For the %sStatic Map API%s this URL is only used when theme is set to %s. Default: %s"), '<a href="https://github.com/wavelog/wavelog/wiki/Static-Map-Images">', '</a>', "'light'", "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"); ?></small>
+                            <small id="maptile_server_urlHelp" class="form-text text-muted"><?= __("URL of the map server which serves the maptiles."); ?></small>
                         </div>
                         <div class="mb-3">
                             <label for="maptile_server_url_dark"><?= __("Maptiles Server URL for Dark Tiles - ONLY Static Map API"); ?></label>
                             <input type="text" name="maptile_server_url_dark" class="form-control" id="maptile_server_url_dark" aria-describedby="maptile_server_url_darkHelp" value="<?php echo $maptile_server_url_dark; ?>">
-                            <small id="maptile_server_url_darkHelp" class="form-text text-muted"><?= sprintf(__("URL of the map server which serves the dark maptiles. Only used for Static Map API. Default: %s"),"https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"); ?></small>
+                            <small id="maptile_server_url_darkHelp" class="form-text text-muted"><?= __("URL of the map server which serves the dark maptiles. Only used for Static Map."); ?></small>
                         </div>
                         <div class="mb-3">
                             <label for="subdomain_system"><?= __("Subdomain System of Maptile Server"); ?></label>
                             <input type="text" name="subdomain_system" class="form-control" id="subdomain_system" aria-describedby="subdomain_systemHelp" value="<?php echo $subdomain_system; ?>">
-                            <small id="subdomain_systemHelp" class="form-text text-muted"><?= sprintf(__("System of the subdomains at this server ({s}). They are used for loadbalancing. Default: %s"), "abc"); ?></small>
+                            <small id="subdomain_systemHelp" class="form-text text-muted"><?= __("System of the subdomains at this server ({s} in the URL). They are used for loadbalancing."); ?></small>
                         </div>
                         <div class="mb-3">
                             <label for="copyright_url"><?= __("URL of the Copyright Source"); ?></label>
                             <input type="text" name="copyright_url" class="form-control" id="copyright_url" aria-describedby="copyright_urlHelp" value="<?php echo $copyright_url; ?>">
-                            <small id="copyright_urlHelp" class="form-text text-muted"><?= sprintf(__("Source URL for the copyright tag. Default: %s"), "https://www.openstreetmap.org/"); ?></small>
+                            <small id="copyright_urlHelp" class="form-text text-muted"><?= __("Source URL for the copyright tag."); ?></small>
                         </div>
                         <div class="mb-3">
                             <label for="copyright_text"><?= __("Name of the Copyright Source"); ?></label>
                             <input type="text" name="copyright_text" class="form-control" id="copyright_text" aria-describedby="copyright_textHelp" value="<?php echo $copyright_text; ?>">
-                            <small id="copyright_textHelp" class="form-text text-muted"><?= sprintf(__("Text for the copyright tag. Default: %s"), "OpenStreetMap"); ?></small>
+                            <small id="copyright_textHelp" class="form-text text-muted"><?= __("Text for the copyright tag."); ?></small>
                         </div>
 
                         <div class="d-flex justify-content-between">
