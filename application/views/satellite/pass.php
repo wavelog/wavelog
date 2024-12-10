f111b1e54f17d97d53ef537b5cae8fabea1a3252
Andreas Kristiansen <6977712+AndreasK79@users.noreply.github.com>
2024-12-10 12:58:48 +0100

    Fixed classes in view

--- application/views/satellite/pass.php
+++ application/views/satellite/pass.php
@@ -9,11 +9,11 @@
 			<h4>Your station</h4>
 			<div class="mb-3 w-auto">
 				<label class="my-1 me-sm-2 w-auto" id="minelevation" for="minelevation"><?= __("Min. Satellite Elevation"); ?></label>
-				<input class="my-1 me-sm-2 w-auto form-control" id="minelevation" type="number" min="0" max="90" name="minelevation" value="0" />
+				<input class="my-1 me-sm-2 w-auto form-control form-control-sm" id="minelevation" type="number" min="0" max="90" name="minelevation" value="0" />
 			</div>
 			<div class="mb-3 w-auto">
 				<label class="my-1 me-sm-2 w-auto" for="minazimuth"><?= __("Min. Azimuth"); ?></label>
-                <select class="my-1 me-sm-2 w-auto form-select" id="minazimuth" name="minazimuth">
+                <select class="my-1 me-sm-2 w-auto form-select form-select-sm" id="minazimuth" name="minazimuth">
 				<?php for ($i = 0; $i <= 350; $i += 10): ?>
 					<option value="<?= $i ?>" <?= $i === 0 ? 'selected' : '' ?>><?= $i ?> &deg;</option>
 				<?php endfor; ?>
@@ -21,7 +21,7 @@
 			</div>
 			<div class="mb-3 w-auto">
 				<label class="my-1 me-sm-2 w-auto" for="maxazimuth"><?= __("Max. Azimuth"); ?></label>
-				<select class="my-1 me-sm-2 w-auto form-select" id="maxazimuth" name="maxazimuth">
+				<select class="my-1 me-sm-2 w-auto form-select form-select-sm" id="maxazimuth" name="maxazimuth">
 					<?php for ($i = 10; $i <= 360; $i += 10): ?>
 						<option value="<?= $i ?>" <?= $i === 360 ? 'selected' : '' ?>><?= $i ?> &deg;</option>
 					<?php endfor; ?>
@@ -29,15 +29,15 @@
 			</div>
 			<div class="mb-3 w-auto">
 					<label class="my-1 me-sm-2 w-auto" for="yourgrid"><?= __("Gridsquare"); ?></label>
-                    <input class="my-1 me-sm-2 w-auto form-control"  id="yourgrid" type="text" name="gridsquare" value="<?php echo $activegrid; ?>"/>
+                    <input class="my-1 me-sm-2 w-auto form-control form-control-sm"  id="yourgrid" type="text" name="gridsquare" value="<?php echo $activegrid; ?>"/>
 			</div>
 			<div class="mb-3 w-auto">
 					<label class="my-1 me-sm-2 w-auto" for="altitude"><?= __("Altitude (meters)"); ?></label>
-                    <input class="my-1 me-sm-2 w-auto form-control"  id="altitude" type="number" name="altitude" value="0" />
+                    <input class="my-1 me-sm-2 w-auto form-control form-control-sm"  id="altitude" type="number" name="altitude" value="0" />
 			</div>
 			<div class="mb-3 w-auto">
 					<label class="my-1 me-sm-2 w-auto" for="timezone"><?= __("Timezone"); ?></label>
-					<select class="my-1 me-sm-2 w-auto form-select" id="timezone" name="timezone">
+					<select class="my-1 me-sm-2 w-auto form-select form-control-sm" id="timezone" name="timezone">
                             <option>Africa/Abidjan</option>
                             <option>Africa/Accra</option>
                             <option>Africa/Addis_Ababa</option>
@@ -478,7 +478,7 @@
 					</div>
 					<div class="mb-3 w-auto">
 						<label class="my-1 me-sm-2 w-auto" for="mintime"><?= __("Min. time"); ?></label>
-						<select class="my-1 me-sm-2 w-auto form-select" id="mintime" name="mintime">
+						<select class="my-1 me-sm-2 w-auto form-select form-select-sm" id="mintime" name="mintime">
                         <?php for ($i = 0; $i <= 24; $i += 1): ?>
                             <option value="<?= $i ?>" <?= $i === 00 ? 'selected' : '' ?>><?= $i ?>:00</option>
                         <?php endfor; ?>
@@ -486,7 +486,7 @@
 				</div>
 				<div class="mb-3 w-auto">
 						<label class="my-1 me-sm-2 w-auto" for="maxtime"><?= __("Max. time"); ?></label>
-						<select class="my-1 me-sm-2 w-auto form-select" id="maxtime" name="maxtime">
+						<select class="my-1 me-sm-2 w-auto form-select form-select-sm" id="maxtime" name="maxtime">
                         <?php for ($i = 0; $i <= 24; $i += 1): ?>
                             <option value="<?= $i ?>" <?= $i === 24 ? 'selected' : '' ?>><?= $i ?>:00</option>
                         <?php endfor; ?>
@@ -494,7 +494,7 @@
                 </div>
                 <div class="mb-3 w-auto">
 					<label class="my-1 me-sm-2 w-auto" id="satslabel" for="satlist"><?= __("Satellites"); ?></label>
-					<select class="my-1 me-sm-2 w-auto form-select"  id="satlist">
+					<select class="my-1 me-sm-2 w-auto form-select form-select-sm"  id="satlist">
 						<?php foreach($satellites as $sat) {
 							echo '<option value="' . $sat->satname . '"' . '>' . $sat->satname . '</option>'."\n";
 						} ?>
@@ -505,11 +505,11 @@
 				<h4>Sked partner</h2>
 					<div class="mb-3 w-auto">
 						<label class="my-1 me-sm-2 w-auto" id="minskedelevationlabel" for="minskedelevation"><?= __("Min. Satellite Elevation"); ?></label>
-						<input class="my-1 me-sm-2 w-auto form-control" id="minskedelevation" type="number" min="0" max="90" name="minskedelevation" value="0" />
+						<input class="my-1 me-sm-2 w-auto form-control form-control-sm" id="minskedelevation" type="number" min="0" max="90" name="minskedelevation" value="0" />
 					</div>
 					<div class="mb-3 w-auto">
 						<label class="my-1 me-sm-2 w-auto" for="minskedazimuth"><?= __("Min. Azimuth"); ?></label>
-						<select class="my-1 me-sm-2 w-auto form-select" id="minskedazimuth" name="minskedazimuth">
+						<select class="my-1 me-sm-2 w-auto form-select form-select-sm" id="minskedazimuth" name="minskedazimuth">
 						<?php for ($i = 0; $i <= 350; $i += 10): ?>
 							<option value="<?= $i ?>" <?= $i === 0 ? 'selected' : '' ?>><?= $i ?> &deg;</option>
 						<?php endfor; ?>
@@ -517,7 +517,7 @@
 					</div>
 					<div class="mb-3 w-auto">
 						<label class="my-1 me-sm-2 w-auto" for="maxskedazimuth"><?= __("Max. Azimuth"); ?></label>
-						<select class="my-1 me-sm-2 w-auto form-select" id="maxskedazimuth" name="maxskedazimuth">
+						<select class="my-1 me-sm-2 w-auto form-select form-select-sm" id="maxskedazimuth" name="maxskedazimuth">
 							<?php for ($i = 10; $i <= 360; $i += 10): ?>
 								<option value="<?= $i ?>" <?= $i === 360 ? 'selected' : '' ?>><?= $i ?> &deg;</option>
 							<?php endfor; ?>
@@ -525,7 +525,7 @@
 					</div>
 					<div class="mb-3 w-auto">
 							<label class="my-1 me-sm-2 w-auto" for="skedgrid"><?= __("Gridsquare"); ?></label>
-							<input class="my-1 me-sm-2 w-auto form-control"  id="skedgrid" type="text" name="skedgrid" value=""/>
+							<input class="my-1 me-sm-2 w-auto form-control form-control-sm"  id="skedgrid" type="text" name="skedgrid" value=""/>
 					</div>
 				</div>
         </form>
