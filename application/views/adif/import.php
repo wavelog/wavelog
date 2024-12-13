e2d1e65d23c479cf0c36b033969e60c124e9c789
HB9HIL <fabian.berg@hb9hil.org>
2024-12-13 08:55:53 +0100

    added new adif fields to import logic

--- application/views/adif/import.php
+++ application/views/adif/import.php
@@ -139,6 +139,16 @@
                         <div class="mb-3 row">
                             <div class="col-md-10">
                                 <div class="form-check-inline">
+                                    <input class="form-check-input" type="checkbox" name="markDcl" value="1" id="markDclImport">
+                                    <label class="form-check-label" for="markDclImport"><?= __("Mark imported QSOs as uploaded to DCL Logbook") ?></label>
+                                </div>
+                                <div class="small form-text text-muted"><?= __("Select if ADIF being imported does not contain this information.") ?></div>
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <div class="col-md-10">
+                                <div class="form-check-inline">
                                     <input class="form-check-input" type="checkbox" name="dxccAdif" value="1" id="dxccAdif">
                                     <label class="form-check-label" for="dxccAdif"><?= __("Use DXCC information from ADIF") ?></label>
                                 </div>
