2f8ee3e796cea881194605fcb54c880db150fe51
Fabian Berg <fabian.berg@hb9hil.org>
2025-01-07 09:58:10 +0100

    catch undefined

--- application/controllers/Staticmap.php
+++ application/controllers/Staticmap.php
@@ -15,7 +15,7 @@ class Staticmap extends CI_Controller {
         $this->load->model('stationsetup_model');
         $this->load->model('visitor_model');
 
-        $slug = $this->security->xss_clean($slug);
+        $slug = $this->security->xss_clean($slug) ?? '';
         if (empty($slug)) {
             show_404(__("Unknown Public Page."));
         }
