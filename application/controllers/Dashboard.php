be0f98239a20b4ef352df61004bfe5fc22e2911c
Filip Melík <filip@melik.cz>
2025-01-09 09:02:58 +0100

    Fix codestyle

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,7 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller
-{
+class Dashboard extends CI_Controller {
 
 	const LAST_QSOS_COUNT = 18; // max number of most recent qsos to be displayed on a dashboard
 
