f7302c341d1fb4e948ba1c6c918ced7b36bcce00
Filip Melík <filip@melik.cz>
2025-01-29 21:09:05 +0100

    Use session value for dashboard last qso count on map, qso list and ux hint

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -114,7 +114,10 @@ class Dashboard extends CI_Controller {
 		$data['qrz_sent_today'] = $QSLStatsBreakdownArray['QRZ_Sent_today'];
 		$data['qrz_rcvd_today'] = $QSLStatsBreakdownArray['QRZ_Received_today'];
 
-		$data['last_qsos_list'] = $this->logbook_model->get_last_qsos('18', $logbooks_locations_array);
+		$data['last_qsos_list'] = $this->logbook_model->get_last_qsos(
+			$this->session->userdata('dashboard_last_qso_count'), 
+			$logbooks_locations_array,
+		);
 
 		$data['vucc'] = $this->vucc->fetchVuccSummary();
 		$data['vuccSAT'] = $this->vucc->fetchVuccSummary('SAT');
