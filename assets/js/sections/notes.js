7f3c0f84188ac98c78742fbfdda2892b06343a52
MatykoBr <mabregner@gmail.com>
2025-02-11 22:19:59 +0100

    Added support for Markdown in Notes as requested in #936
    Added EasyMDE instead of Quill as the editor, because it supports Markdown (requested in #936 ). Made the colors of the editor fit the colors of the selected theme.

--- assets/js/sections/notes.js
+++ assets/js/sections/notes.js
@@ -1,8 +1,68 @@
-var quill = new Quill('#quillArea', { 
-	placeholder: 'Compose an epic...',
-	theme: 'snow'
+const notes = new EasyMDE({element: document.getElementById('notes'), forceSync: true, spellChecker: false, placeholder: 'Gebe die Notiz ein...', maxHeight: '350px', 
+    toolbar: [
+        {
+            name: "bold",
+            action: EasyMDE.toggleBold,
+            className: "fa fa-bold",
+            title: "Fett",
+        },
+        {
+            name: "italic",
+            action: EasyMDE.toggleItalic,
+            className: "fa fa-italic",
+            title: "Kursiv",
+        },
+        {
+            name: "heading",
+            action: EasyMDE.toggleHeadingSmaller,
+            className: "fa fa-header",
+            title: "Überschrift"
+        },
+        "|",
+        {
+            name: "quote",
+            action: EasyMDE.toggleBlockquote,
+            className: "fa fa-quote-left",
+            title: "Zitat"
+        },
+        {
+            name: "unordered-list",
+            action: EasyMDE.toggleUnorderedList,
+            className: "fa fa-list-ul",
+            title: "Unsortierte Liste"
+        },
+        {
+            name: "ordered-list",
+            action: EasyMDE.toggleOrderedList,
+            className: "fa fa-list-ol",
+            title: "Sortierte Liste"
+        },
+        "|",
+        {
+            name: "link",
+            action: EasyMDE.drawLink,
+            className: "fa fa-link",
+            title: "Link"
+        },
+        {
+            name: "image",
+            action: EasyMDE.drawImage,
+            className: "fa fa-image",
+            title: "Bild"
+        },
+        "|",
+        {
+            name: "preview",
+            action: EasyMDE.togglePreview,
+            className: "fa fa-eye no-disable",
+            title: "Vorschau"
+        },
+        "|",
+        {
+            name: "guide",
+            action: "https://www.markdownguide.org/basic-syntax/", // link
+            className: "fa fa-question-circle",
+            title: "Markdown Guide"
+        }
+    ]
 });
-
-$("#notes_add").on("submit",function(){
-	$("#hiddenArea").val(quill.root.innerHTML);
-})
