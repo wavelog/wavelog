3457889a24d313b42972a1c2297b7816c16fc487
phl0 <github@florian-wolters.de>
2025-02-21 09:30:39 +0100

    Mark QSOs with prop mode SAT but empty SAT name as invalid for LoTW uploads

--- application/models/Logbook_model.php
+++ application/models/Logbook_model.php
@@ -5374,8 +5374,15 @@ class Logbook_model extends CI_Model {
 		);
 		$this->db->where("station_id", $station_id);
 		$this->db->group_start();
-		$this->db->where_in('COL_PROP_MODE', $this->config->item('lotw_unsupported_prop_modes'));
-		$this->db->or_where_in('COL_SAT_NAME', $invalid_sats);
+			$this->db->where_in('COL_PROP_MODE', $this->config->item('lotw_unsupported_prop_modes'));
+			$this->db->or_where_in('COL_SAT_NAME', $invalid_sats);
+			$this->db->or_group_start();
+				$this->db->where('COL_PROP_MODE', 'SAT');
+				$this->db->group_start();
+					$this->db->where('COL_SAT_NAME', '');
+					$this->db->or_where('COL_SAT_NAME', null);
+				$this->db->group_end();
+			$this->db->group_end();
 		$this->db->group_end();
 		$this->db->update($this->config->item('table_name'), $data);
 	}
