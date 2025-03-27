8177b89771edd3d9401d11353988115148371679
int2001 <joerg@dj7nt.de>
2025-03-27 11:59:07 +0000

    Added link to wiki for format

--- application/views/options/hon.php
+++ application/views/options/hon.php
@@ -36,7 +36,7 @@
 
                         <div class="mb-3">
                             <label for="globalSearch"><?= __("Provider for Hams Of Note"); ?></label>
-                            <p><?= __("The URL which provides the Hams Of Note List"); ?></p>
+                            <p><?= sprintf(__("The URL which provides the Hams Of Note List. See example and how it works here %s"),'<a href="https://github.com/wavelog/wavelog/wiki/Hams-Of-Note">'.__("Wiki")."<a/>"); ?></p>
                             <input type="text" name="hon_url" class="form-control" id="dxcache_url" aria-describedby="hon_urlHelp" value="<?php echo $this->optionslib->get_option('hon_url'); ?>">
                             <small id="hon_urlHelp" class="form-text text-muted"><?= sprintf(__("URL of the Hams Of Note List. e.g. %s"), "https://api.ham2k.net/data/ham2k/hams-of-note.txt" ); ?></small>
                         </div>
