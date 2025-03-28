f5a10cc60771ce7d1be0f10d0a85e3fe9cc2149e
int2001 <joerg@dj7nt.de>
2025-03-28 06:41:09 +0000

    order by description to make result at least reproduceable for dupes

--- application/models/Pota.php
+++ application/models/Pota.php
@@ -26,6 +26,7 @@ class Pota extends CI_Model {
 
 	function ham_of_note($callsign) {
 		$this->db->where('callsign', $callsign);
+		$this->db->order_by('description asc');
 		$this->db->limit(1);
 
 		return $this->db->get('hams_of_note');
