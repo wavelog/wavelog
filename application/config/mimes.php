6d8f843d439cd64ed257ba9965bcddf0542b3e3b
int2001 <joerg@dj7nt.de>
2025-05-17 05:45:11 +0000

    Only ZZIP was / is missing

--- application/config/mimes.php
+++ application/config/mimes.php
@@ -10,10 +10,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 return array(
-	'adi' => array('application/octet-stream','text/plain', 'audio/x-hx-aac-adif'),
-	'adif' => array('application/octet-stream','text/plain', 'audio/x-hx-aac-adif'),
-	'ADI' => array('application/octet-stream','text/plain', 'audio/x-hx-aac-adif'),
-	'ADIF' => array('application/octet-stream','text/plain', 'audio/x-hx-aac-adif'),
 	'hqx'	=>	array('application/mac-binhex40', 'application/mac-binhex', 'application/x-binhex40', 'application/x-mac-binhex40'),
 	'cpt'	=>	'application/mac-compactpro',
 	'csv'	=>	array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain'),
