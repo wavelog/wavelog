5363d69533e57446da6c1bbb59ffd3946a966f05
phl0 <github@florian-wolters.de>
2025-05-29 10:50:33 +0200

    Add distance to table

--- application/views/statistics/initialresult.php
+++ application/views/statistics/initialresult.php
@@ -18,6 +18,7 @@ if ($intials_array) {
 						<th>' . __("Band") . '</th>
 						<th>' . __("Mode") . '</th>
 						<th>' . __("Gridsquare") . '</th>
+						<th>' . __("Distance") . '</th>
                     </tr>
                 </thead>
                 <tbody>';
@@ -32,6 +33,7 @@ if ($intials_array) {
                 <td>' . $line->col_band . '</td>
                 <td>' . (empty($line->col_submode) ? ($line->col_mode ?? '') : $line->col_submode) . '</td>
 				<td>' . (empty($line->col_vucc_grids) ? ($line->col_gridsquare ?? '') : $line->col_vucc_grids) . '</td>
+                <td>' . $line->col_distance . ' km</td>
 		</tr>';
 	}
 
