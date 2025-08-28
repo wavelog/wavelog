126fcc9863d50f9dcae99bbf5c4d878581facf79
Andreas Kristiansen <6977712+AndreasK79@users.noreply.github.com>
2025-08-28 20:36:39 +0200

    Needs seconds here, fails for me otherwise

--- application/models/Contesting_model.php
+++ application/models/Contesting_model.php
@@ -18,7 +18,7 @@ class Contesting_model extends CI_Model {
 		$qsoarray = explode(',', $this->security->xss_clean($qso));
 
 		$contestid = $qsoarray[2];
-		$date = DateTime::createFromFormat('Y-m-d H:i', $qsoarray[0]);
+		$date = DateTime::createFromFormat('Y-m-d H:i:s', $qsoarray[0]);
 		if ($date == false) $date = DateTime::createFromFormat($date_format.' H:i', $qsoarray[0]);
 		$date = $date->format('Y-m-d H:i:s');
 
