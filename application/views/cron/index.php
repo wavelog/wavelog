01971e8f5a9604c649a5194d4a9d87c52cb5c395
phl0 <github@florian-wolters.de>
2025-09-16 12:32:15 +0200

    Rename Wiki :)

--- application/views/cron/index.php
+++ application/views/cron/index.php
@@ -18,7 +18,7 @@
 						<strong class="d-block"><i class="fas fa-info-circle me-2"></i> Important</strong>
 
 						<div>
-							<?= sprintf(__("For more information or help, take a look in the %s."), '<a target="_blank" class="text-primary fw-bold text-decoration-underline" href="https://github.com/wavelog/wavelog/wiki/Recommended-Cron-Jobs-and-Cronmanager">'.'WIKI'.'</a>'); ?>
+							<?= sprintf(__("For more information or help, take a look in the %s."), '<a target="_blank" class="text-primary fw-bold text-decoration-underline" href="https://github.com/wavelog/wavelog/wiki/Recommended-Cron-Jobs-and-Cronmanager">'.'Wiki'.'</a>'); ?>
 						</div>
 					</div>
 				</div>
