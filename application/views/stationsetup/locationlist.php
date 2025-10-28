2eaaaa4fbd64e9212f0cb6c6d2630ffffe74ac1d
Andreas Kristiansen <6977712+AndreasK79@users.noreply.github.com>
2025-10-28 08:28:10 +0100

    Prettified table a bit

--- application/views/stationsetup/locationlist.php
+++ application/views/stationsetup/locationlist.php
@@ -1,27 +1,79 @@
 <h1><?= $page_title ?></h1>
 
-<?php if (!empty($locations)): ?>
-    <table style="width:100%" class="table-sm table table-bordered table-hover table-striped table-condensed text-center">
-        <thead>
-            <tr>
-                <?php
-                // Dynamically create table headers based on object keys
-                $first_row = (array)$locations[0];
-                foreach (array_keys($first_row) as $key): ?>
-                    <th><?= ucfirst(str_replace('_', ' ', $key)) ?></th>
-                <?php endforeach; ?>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($locations as $loc): ?>
-                <tr>
-                    <?php foreach ((array)$loc as $val): ?>
-                        <td><?php echo $val; ?></td>
+    <?php if (!empty($locations)): ?>
+        <div class="table-responsive mt-3">
+			<table style="width:100%" class="table-sm table table-bordered table-hover table-striped table-condensed text-center">
+                <thead class="table-primary">
+                    <tr>
+                        <th>Name</th>
+                        <th>Grid</th>
+                        <th>City</th>
+                        <th>IOTA</th>
+                        <th>SOTA</th>
+                        <th>Callsign</th>
+                        <th>Power</th>
+                        <th>DXCC</th>
+                        <th>County</th>
+                        <th>CQ Zone</th>
+                        <th>ITU Zone</th>
+                        <th>Active</th>
+                        <th>eQSL Nick</th>
+                        <th>State</th>
+                        <th>County</th>
+                        <th>SIG</th>
+                        <th>SIG Info</th>
+                        <th>QRZ Live</th>
+                        <th>WWFF</th>
+                        <th>POTA</th>
+                        <th>OQRS</th>
+                        <th>OQRS Text</th>
+                        <th>OQRS Email</th>
+                        <th>Web ADIF Live</th>
+                        <th>ClubLog Live</th>
+                        <th>ClubLog Ignore</th>
+                        <th>HRDLog Live</th>
+                        <th>Created</th>
+                        <th>Last Modified</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($locations as $loc): ?>
+                        <tr>
+                            <td><?php echo $loc->station_profile_name; ?></td>
+                            <td><?php echo $loc->station_gridsquare; ?></td>
+                            <td><?php echo $loc->station_city; ?></td>
+                            <td><?php echo $loc->station_iota; ?></td>
+                            <td><?php echo $loc->station_sota; ?></td>
+                            <td><?php echo $loc->station_callsign; ?></td>
+                            <td><?php echo $loc->station_power; ?></td>
+                            <td><?php echo $loc->station_dxcc; ?></td>
+                            <td><?php echo $loc->station_cnty; ?></td>
+                            <td><?php echo $loc->station_cq; ?></td>
+                            <td><?php echo $loc->station_itu; ?></td>
+                            <td><?php echo $loc->station_active ? 'Yes' : 'No' ?></td>
+                            <td><?php echo $loc->eqslqthnickname; ?></td>
+                            <td><?php echo $loc->state; ?></td>
+                            <td><?php echo $loc->county; ?></td>
+                            <td><?php echo $loc->station_sig; ?></td>
+                            <td><?php echo $loc->station_sig_info; ?></td>
+                            <td><?php echo $loc->qrzrealtime ? 'Yes' : 'No'; ?></td>
+                            <td><?php echo ($loc->station_wwff); ?></td>
+                            <td><?php echo ($loc->station_pota); ?></td>
+                            <td><?php echo $loc->oqrs ? 'Yes' : 'No'; ?></td>
+                            <td><?php echo $loc->oqrs_text; ?></td>
+                            <td><?php echo $loc->oqrs_email; ?></td>
+                            <td><?php echo $loc->webadifrealtime ? 'Yes' : 'No' ?></td>
+                            <td><?php echo $loc->clublogrealtime ? 'Yes' : 'No' ?></td>
+                            <td><?php echo $loc->clublogignore ? 'Yes' : 'No' ?></td>
+                            <td><?php echo $loc->hrdlogrealtime ? 'Yes' : 'No' ?></td>
+                            <td><?php echo $loc->creation_date; ?></td>
+                            <td><?php echo $loc->last_modified; ?></td>
+                        </tr>
                     <?php endforeach; ?>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-<?php else: ?>
-    <div class="no-data">No locations found.</div>
-<?php endif; ?>
+                </tbody>
+            </table>
+        </div>
+    <?php else: ?>
+        <div class="alert alert-info mt-3">No station locations found.</div>
+    <?php endif; ?>
+</div>
