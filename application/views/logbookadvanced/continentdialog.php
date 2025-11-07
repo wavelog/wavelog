51e62f4d8c04a27788615084cbd4c55be4c1f324
Andreas Kristiansen <6977712+AndreasK79@users.noreply.github.com>
2025-11-07 14:16:25 +0100

    Translation in the dialog

--- application/views/logbookadvanced/continentdialog.php
+++ application/views/logbookadvanced/continentdialog.php
@@ -1,5 +1,5 @@
 <div class="container-fluid">
-	Update all QSOs with the continent based on the DXCC country of the QSO.
-	This is useful if you have imported QSOs without continent information.<br /><br />
-	Update will only set the continent for QSOs where the continent is empty or invalid (not AF, AN, AS, EU, NA, OC or SA).
+	<?= __("Update all QSOs with the continent based on the DXCC country of the QSO."); ?>
+	<?= __("This is useful if you have imported QSOs without continent information."); ?><br /><br />
+	<?= __("Update will only set the continent for QSOs where the continent is empty."); ?>
 </div>
