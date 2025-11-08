0784bff535ab9522414a7315c1bbc1c55faaec85
Andreas Kristiansen <6977712+AndreasK79@users.noreply.github.com>
2025-11-09 00:13:08 +0100

    Added support for translation in the distance dialog

--- application/views/logbookadvanced/distancedialog.php
+++ application/views/logbookadvanced/distancedialog.php
@@ -1,5 +1,5 @@
 <div class="container-fluid">
-	Update all QSOs with the distance based on your gridsquare set in the station profile, and the gridsquare of the QSO partner. Distance will be calculated based on if short path or long path is set.
-	This is useful if you have imported QSOs without distance information.<br /><br />
-	Update will only set the distance for QSOs where the distance is empty.
+	<?= __("Update all QSOs with the distance based on your gridsquare set in the station profile, and the gridsquare of the QSO partner. Distance will be calculated based on if short path or long path is set."); ?>
+	<?= __("This is useful if you have imported QSOs without distance information."); ?><br /><br />
+	<?= __("Update will only set the distance for QSOs where the distance is empty."); ?>
 </div>
