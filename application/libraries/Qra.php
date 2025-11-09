4f642df95e29b814488a2f7177daf172030286a6
phl0 <github@florian-wolters.de>
2025-11-10 00:14:31 +0100

    Stray empty line ...

--- application/libraries/Qra.php
+++ application/libraries/Qra.php
@@ -219,7 +219,6 @@ class Qra {
 		else if (preg_match('/^[A-Ra-r]{2}[0-9]{2}[A-Za-z]{2}[0-9]{2}$/', $grid)) return true;
 		// Allow 10-digit locator
 		else if (preg_match('/^[A-Ra-r]{2}[0-9]{2}[A-Za-z]{2}[0-9]{2}[A-Za-z]{2}$/', $grid)) return true;
-
 		return false;
 	}
 }
