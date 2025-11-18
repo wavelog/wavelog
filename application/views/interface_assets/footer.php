f80758c7f7d65236b0b3daf861cc9e702150736a
Szymon Porwolik <szporwolik@gmail.com>
2025-11-18 17:21:29 +0100

    Removed debug code

--- application/views/interface_assets/footer.php
+++ application/views/interface_assets/footer.php
@@ -1445,15 +1445,6 @@ mymap.on('mousemove', onQsoMapMove);
 
 <?php } ?>
 
-<!--- Bandmap CAT Integration --->
-<?php if ($this->uri->segment(1) == "bandmap" && $this->uri->segment(2) == "list") { ?>
-	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/cat.js?v=<?php echo time(); ?>"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/leaflet/leaflet.geodesic.js?v=<?php echo time(); ?>"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/leaflet.polylineDecorator.js?v=<?php echo time(); ?>"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/leaflet/L.Terminator.js?v=<?php echo time(); ?>"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/sections/bandmap_list.js?v=<?php echo time(); ?>"></script>
-<?php } ?>
-
 <?php if ($this->uri->segment(1) == "logbook" && $this->uri->segment(2) == "view") { ?>
 <script>
 
