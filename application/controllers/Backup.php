1b2bb022e3bb0e911f7b7f1151cc548e38ee93b2
int2001 <joerg@dj7nt.de>
2025-12-05 06:02:17 +0000

    Use Chunking for Backup as well

--- application/controllers/Backup.php
+++ application/controllers/Backup.php
@@ -29,31 +29,43 @@ class Backup extends CI_Controller {
 		$clean_key = $this->security->xss_clean($key);
 
 		$this->load->helper('file');
+		$this->load->library('AdifHelper');
 		// Set memory limit to unlimited to allow heavy usage
 		ini_set('memory_limit', '-1');
 		
 		$this->load->model('adif_data');
+		$filename = 'backup/logbook'. date('_Y_m_d_H_i_s') .'.adi';
 
-		$data['qsos'] = $this->adif_data->export_all($clean_key);
+		header('Content-Type: text/plain; charset=utf-8');
+		header('Content-Disposition: attachment; filename="'.$filename.'"');
 
-		$data['filename'] = 'backup/logbook'. date('_Y_m_d_H_i_s') .'.adi';
-		
-		if ( ! write_file($data['filename'], $this->load->view('backup/exportall', $data, true)))
-		{
-			$data['status'] = false;
-		}
-		else
-		{
-			$data['status'] = true;
-		}
+		// Output ADIF header // No chance to use exportall-view any longer, because of chunking logic
+		echo "Wavelog ADIF export\n";
+		echo "<ADIF_VER:5>3.1.6\n";
+		echo "<PROGRAMID:".strlen($this->config->item('app_name')).">".$this->config->item('app_name')."\r\n";
+		echo "<PROGRAMVERSION:".strlen($this->optionslib->get_option('version')).">".$this->optionslib->get_option('version')."\r\n";
+		echo "<EOH>\n\n";
 
-		$data['page_title'] = __("ADIF - Backup");
-		
+		// Stream QSOs in 5K chunks
+		$offset = 0;
+		$chunk_size = 5000;
 
-		$this->load->view('interface_assets/header', $data);
-		$this->load->view('backup/adif_view');
-		$this->load->view('interface_assets/footer');
+		do {
+			$qsos = $this->adif_data->export_all_chunked($clean_key, null, null, false, null, $offset, $chunk_size);
 
+			if ($qsos->num_rows() > 0) {
+				foreach ($qsos->result() as $qso) {
+					echo $this->adifhelper->getAdifLine($qso);
+				}
+
+				// Free memory
+				$qsos->free_result();
+			}
+
+			$offset += $chunk_size;
+		} while ($qsos->num_rows() > 0);
+
+		exit;
 	}
 
 	/* Export the notes to XML */
