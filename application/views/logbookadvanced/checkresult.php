e2194257ff8ccd96e5b7a08ffd6a95484033999c
Andreas Kristiansen <6977712+AndreasK79@users.noreply.github.com>
2025-12-08 20:58:39 +0100

    Added Run buttons

--- application/views/logbookadvanced/checkresult.php
+++ application/views/logbookadvanced/checkresult.php
@@ -23,24 +23,44 @@ switch ($type) {
 function check_missing_distance($result) { ?>
 	<h5>Distance Check Results</h5>
 	QSOs to update found: <?php echo $result[0]->count; ?>
+	<br/>
+	<button type="button" class="mt-2 btn btn-sm btn-primary ld-ext-right" id="updateDistancesBtn" onclick="fixMissingDxcc('All')">
+		<?= __("Run fix") ?><div class="ld ld-ring ld-spin"></div>
+	</button>
 <?php }
 
 function check_qsos_missing_continent($result) { ?>
 	<h5>Continent Check Results</h5>
 	QSOs to update found: <?php echo $result[0]->count; ?>
+	<br/>
+	<button type="button" class="mt-2 btn btn-sm btn-primary ld-ext-right" id="updateDistancesBtn" onclick="fixMissingDxcc('All')">
+		<?= __("Run fix") ?><div class="ld ld-ring ld-spin"></div>
+	</button>
 <?php }
 
 function check_missing_dxcc($result) { ?>
 	<h5>DXCC Check Results</h5>
 	QSOs to update found: <?php echo $result[0]->count; ?>
+	<br/>
+	<button type="button" class="mt-2 btn btn-sm btn-primary ld-ext-right" id="updateDistancesBtn" onclick="fixMissingDxcc('All')">
+		<?= __("Run fix") ?><div class="ld ld-ring ld-spin"></div>
+	</button>
 <?php }
 
 function check_missing_cq_zones($result) { ?>
 	<h5>CQ Zone Check Results</h5>
 	QSOs to update found: <?php echo $result[0]->count; ?>
+	<br/>
+	<button type="button" class="mt-2 btn btn-sm btn-primary ld-ext-right" id="updateDistancesBtn" onclick="fixMissingDxcc('All')">
+		<?= __("Run fix") ?><div class="ld ld-ring ld-spin"></div>
+	</button>
 <?php }
 
 function check_missing_itu_zones($result) { ?>
 	<h5>ITU Zone Check Results</h5>
 	QSOs to update found: <?php echo $result[0]->count; ?>
+	<br/>
+	<button type="button" class="mt-2 btn btn-sm btn-primary ld-ext-right" id="updateDistancesBtn" onclick="fixMissingDxcc('All')">
+		<?= __("Run fix") ?><div class="ld ld-ring ld-spin"></div>
+	</button>
 <?php }
