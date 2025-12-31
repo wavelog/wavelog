b33eb9d14397cca9f156ff26fd81e00c4ecc7274
Andreas Kristiansen <6977712+AndreasK79@users.noreply.github.com>
2025-12-31 16:31:47 +0100

    Fix for dxcc table too

--- assets/js/sections/logbookadvanced.js
+++ assets/js/sections/logbookadvanced.js
@@ -2796,8 +2796,8 @@ function saveOptions() {
 				id_list.forEach(function(id) {
 					let row = $("#dxccCheckTable tbody tr#qsoID-" + id);
 					table.row(row).remove();
-					table.draw(false);
 				});
+				table.draw(false);
 				$('.dxcctablediv').html(data.message);
 			},
 			error: function(xhr, status, error) {
