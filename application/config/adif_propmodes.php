315f21066d6e1b579c48f7b94528e60efdaaef7c
Florian (DF2ET) <github@florian-wolters.de>
2026-05-08 11:26:05 +0200

    Update application/config/adif_propmodes.php
    Co-authored-by: Fabian Berg <fabian.berg@hb9hil.org>

--- application/config/adif_propmodes.php
+++ application/config/adif_propmodes.php
@@ -4,26 +4,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /* Centralized definition of (valid) modes defined in ADIF specification */
 
 $config['adif_propmodes'] = array(
-   "AS" => __("Aircraft Scatter"),
-   "AUR" => __("Aurora"),
-   "AUE" => __("Aurora-E"),
-   "BS" => __("Back scatter"),
-   "ECH" => __("EchoLink"),
-   "EME" => __("Earth-Moon-Earth"),
-   "ES" => __("Sporadic E"),
-   "FAI" => __("Field Aligned Irregularities"),
-   "F2" => __("F2 Reflection"),
-   "GWAVE" => __("Ground Wave"),
-   "INTERNET" => __("Internet-assisted"),
-   "ION" => __("Ionoscatter"),
-   "IRL" => __("IRLP"),
-   "LOS" => __("Line of Sight (includes transmission through obstacles such as walls)"),
-   "MS" => __("Meteor scatter"),
-   "RPT" => __("Terrestrial or atmospheric repeater or transponder"),
-   "RS" => __("Rain scatter"),
-   "SAT" => __("Satellite"),
-   "TEP" => __("Trans-equatorial"),
-   "TR" => __("Tropospheric ducting")
+   "AS" => _pgettext("Propagation Mode", "Aircraft Scatter"),
+   "AUR" => _pgettext("Propagation Mode", "Aurora"),
+   "AUE" => _pgettext("Propagation Mode", "Aurora-E"),
+   "BS" => _pgettext("Propagation Mode", "Back scatter"),
+   "ECH" => _pgettext("Propagation Mode", "EchoLink"),
+   "EME" => _pgettext("Propagation Mode", "Earth-Moon-Earth"),
+   "ES" => _pgettext("Propagation Mode", "Sporadic E"),
+   "FAI" => _pgettext("Propagation Mode", "Field Aligned Irregularities"),
+   "F2" => _pgettext("Propagation Mode", "F2 Reflection"),
+   "GWAVE" => _pgettext("Propagation Mode", "Ground Wave"),
+   "INTERNET" => _pgettext("Propagation Mode", "Internet-assisted"),
+   "ION" => _pgettext("Propagation Mode", "Ionoscatter"),
+   "IRL" => _pgettext("Propagation Mode", "IRLP"),
+   "LOS" => _pgettext("Propagation Mode", "Line of Sight (includes transmission through obstacles such as walls)"),
+   "MS" => _pgettext("Propagation Mode", "Meteor scatter"),
+   "RPT" => _pgettext("Propagation Mode", "Terrestrial or atmospheric repeater or transponder"),
+   "RS" => _pgettext("Propagation Mode", "Rain scatter"),
+   "SAT" => _pgettext("Propagation Mode", "Satellite"),
+   "TEP" => _pgettext("Propagation Mode", "Trans-equatorial"),
+   "TR" => _pgettext("Propagation Mode", "Tropospheric ducting")
 );
 
 ?>
