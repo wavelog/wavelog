6a32fee58801661fb9d7bfb903ce76bc35d0babf
int2001 <joerg@dj7nt.de>
2026-05-28 10:43:14 +0000

    Link directly into streaks-tab

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -296,7 +296,7 @@ function echo_table_header_col($name) {
 		<div class="col-6 col-md-4 col-lg-2">
 			<div class="card h-100">
 				<div class="card-header py-2">
-					<h6 class="mb-0 text-nowrap"><i class="fas fa-fire"></i> <?= __("Current Streak"); ?> <a href="<?php echo site_url('dayswithqso'); ?>"><i class="fa-solid fa-up-right-from-square"></i></a></h6>
+					<h6 class="mb-0 text-nowrap"><i class="fas fa-fire"></i> <?= __("Current Streak"); ?> <a href="<?php echo site_url('dayswithqso'); ?>#streaks"><i class="fa-solid fa-up-right-from-square"></i></a></h6>
 				</div>
 				<div class="card-body p-0">
 					<h4 class="fw-bold mb-0 px-3 py-2"><?php echo $current_streak . ' ' . __("Days"); ?></h4>
