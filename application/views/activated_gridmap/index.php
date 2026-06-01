5051e8cef09fe302cbbaf31f79aba005ef1fc157
Andreas Kristiansen <6977712+AndreasK79@users.noreply.github.com>
2026-06-01 18:43:52 +0200

    Fix view

--- application/views/activated_gridmap/index.php
+++ application/views/activated_gridmap/index.php
@@ -13,7 +13,7 @@
 
 			<form class="d-flex flex-wrap align-items-center gap-1">
 				<label class="my-1 me-2" for="band"><?= __("Band"); ?></label>
-				<select class="form-select form-select-sm my-1 me-sm-2 w-auto"  id="band">
+				<select class="form-select form-select-sm my-1 me-sm-2 w-auto" id="band">
 					<option value="All"><?= __("All")?></option>
 					<?php foreach($bands as $band) {
 						echo '<option value="'.$band.'"';
@@ -25,93 +25,91 @@
 				</select>
 				<?php if (count($sats_available) != 0) { ?>
 					<label class="my-1 me-2" for="distplot_sats" id="satslabel" <?php if ($user_default_band != "SAT") { ?>style="display: none;"<?php } ?>><?= __("Satellite"); ?></label>
-					<select class="form-select form-select-sm my-1 me-sm-2 w-auto"  id="sats" <?php if ($user_default_band != "SAT") { ?>style="display: none;"<?php } ?>>
+					<select class="form-select form-select-sm my-1 me-sm-2 w-auto" id="sats" <?php if ($user_default_band != "SAT") { ?>style="display: none;"<?php } ?>>
 						<option value="All"><?= __("All")?></option>
 						<?php foreach($sats_available as $sat) {
 							echo '<option value="' . $sat . '"' . '>' . $sat . '</option>'."\n";
 						} ?>
 					</select>
 				<?php } else { ?>
-					<input id="sats" type="hidden" value="All"></input>
+					<input id="sats" type="hidden" value="All">
 				<?php } ?>
-					<label class="my-1 me-2" id="orbitslabel" for="orbits" <?php if ($user_default_band != "SAT") { ?>style="display: none;"<?php } ?>><?= __("Orbit"); ?></label>
-					<select class="form-select form-select-sm my-1 me-sm-2 w-auto"  id="orbits" <?php if ($user_default_band != "SAT") { ?>style="display: none;"<?php } ?>>
-						<option value="All"><?= __("All")?></option>
-						<?php
-						foreach($orbits as $orbit){
-							echo '<option value="' . $orbit . '">' . strtoupper($orbit) . '</option>'."\n";
-						}
-						?>
+				<label class="my-1 me-2" id="orbitslabel" for="orbits" <?php if ($user_default_band != "SAT") { ?>style="display: none;"<?php } ?>><?= __("Orbit"); ?></label>
+				<select class="form-select form-select-sm my-1 me-sm-2 w-auto" id="orbits" <?php if ($user_default_band != "SAT") { ?>style="display: none;"<?php } ?>>
+					<option value="All"><?= __("All")?></option>
+					<?php foreach($orbits as $orbit) {
+						echo '<option value="' . $orbit . '">' . strtoupper($orbit) . '</option>'."\n";
+					} ?>
 				</select>
 				<label class="my-1 me-2" for="mode"><?= __("Mode"); ?></label>
-				<select class="form-select form-select-sm my-1 me-sm-2 w-auto"  id="mode">
-				<option value="All"><?= __("All")?></option>
-						<?php
-						foreach($modes as $mode){
-							if ($mode->submode ?? '' == '') {
-								echo '<option value="' . $mode . '">' . strtoupper($mode) . '</option>'."\n";
-							}
+				<select class="form-select form-select-sm my-1 me-sm-2 w-auto" id="mode">
+					<option value="All"><?= __("All")?></option>
+					<?php foreach($modes as $mode) {
+						if ($mode->submode ?? '' == '') {
+							echo '<option value="' . $mode . '">' . strtoupper($mode) . '</option>'."\n";
 						}
-						?>
+					} ?>
 				</select>
 				<label class="my-1 me-2"><?= __("Confirmation"); ?></label>
-					<div>
-						<div class="form-check-inline">
+				<div>
+					<div class="form-check form-check-inline">
 						<?php echo '<input class="form-check-input" type="checkbox" name="qsl" id="qsl"';
-							if (isset($user_default_confirmation) && strpos($user_default_confirmation, 'Q') !== false) {
-								echo ' checked' ;
-							}
-							echo '>'; ?>
-							<label class="form-check-label" for="qsl"><?= __("QSL"); ?></label>
-						</div>
-						<div class="form-check-inline">
-						<?php echo '<input class="form-check-input" type="checkbox" name="lotw" id="lotw"';
-							if (isset($user_default_confirmation) && strpos($user_default_confirmation, 'L') !== false) {
-								echo ' checked' ;
-							}
-							echo '>'; ?>
-							<label class="form-check-label" for="lotw"><?= __("LoTW"); ?></label>
-						</div>
-						<div class="form-check-inline">
-						<?php echo '<input class="form-check-input" type="checkbox" name="eqsl" id="eqsl"';
-							if (isset($user_default_confirmation) && strpos($user_default_confirmation, 'E') !== false) {
-								echo ' checked' ;
-							}
-							echo '>'; ?>
-							<label class="form-check-label" for="eqsl"><?= __("eQSL"); ?></label>
-						</div>
-						<div class="form-check-inline">
-						<?php echo '<input class="form-check-input" type="checkbox" name="qrz" id="qrz"';
-							if (isset($user_default_confirmation) && strpos($user_default_confirmation, 'Z') !== false) {
-								echo ' checked' ;
-							}
-							echo '>'; ?>
-							<label class="form-check-label" for="qrz"><?= __("QRZ.com"); ?></label>
-						</div>
+						if (isset($user_default_confirmation) && strpos($user_default_confirmation, 'Q') !== false) {
+							echo ' checked';
+						}
+						echo '>'; ?>
+						<label class="form-check-label" for="qsl"><?= __("QSL"); ?></label>
 					</div>
+					<div class="form-check form-check-inline">
+						<?php echo '<input class="form-check-input" type="checkbox" name="lotw" id="lotw"';
+						if (isset($user_default_confirmation) && strpos($user_default_confirmation, 'L') !== false) {
+							echo ' checked';
+						}
+						echo '>'; ?>
+						<label class="form-check-label" for="lotw"><?= __("LoTW"); ?></label>
+					</div>
+					<div class="form-check form-check-inline">
+						<?php echo '<input class="form-check-input" type="checkbox" name="eqsl" id="eqsl"';
+						if (isset($user_default_confirmation) && strpos($user_default_confirmation, 'E') !== false) {
+							echo ' checked';
+						}
+						echo '>'; ?>
+						<label class="form-check-label" for="eqsl"><?= __("eQSL"); ?></label>
+					</div>
+					<div class="form-check form-check-inline">
+						<?php echo '<input class="form-check-input" type="checkbox" name="qrz" id="qrz"';
+						if (isset($user_default_confirmation) && strpos($user_default_confirmation, 'Z') !== false) {
+							echo ' checked';
+						}
+						echo '>'; ?>
+						<label class="form-check-label" for="qrz"><?= __("QRZ.com"); ?></label>
+					</div>
+				</div>
 
 				<button id="plot" type="button" name="plot" class="btn btn-sm btn-primary me-1 ld-ext-right ld-ext-right-plot" onclick="gridPlot(this.form,<?php echo $visitor == true ? "true" : "false"; ?>)"><?= __("Plot"); ?><div class="ld ld-ring ld-spin"></div></button>
 				<button id="clear" type="button" name="clear" class="btn btn-sm btn-primary me-1 ld-ext-right ld-ext-right-clear" onclick="clearMarkers()"><?= __("Clear Markers"); ?><div class="ld ld-ring ld-spin"></div></button>
 			</form>
 
-		<?php if($this->session->flashdata('message')) { ?>
-			<!-- Display Message -->
-			<div class="alert-message error">
-				<p><?php echo $this->session->flashdata('message'); ?></p>
-			</div>
-		<?php } ?>
+			<?php if($this->session->flashdata('message')) { ?>
+				<!-- Display Message -->
+				<div class="alert-message error">
+					<p><?php echo $this->session->flashdata('message'); ?></p>
+				</div>
+			<?php } ?>
 		</div>
-            <div id="gridmapcontainer">
-                <div id="gridsquare_map" class="map-leaflet" style="width: 100%;"></div>
-            </div>
+		<div id="gridmapcontainer">
+			<div id="gridsquare_map" class="map-leaflet" style="width: 100%;"></div>
+		</div>
+		<div class="card-body">
 			<div class="coordinates" style="position: static;">
-                <div class="cohidden coord-pair"><span><?= __("Latitude") ?>:&nbsp;</span><span class="text-success fw-bold" id="latDeg"></span></div>
-                <div class="cohidden coord-pair"><span><?= __("Longitude") ?>:&nbsp;</span><span class="text-success fw-bold" id="lngDeg"></span></div>
-                <div class="cohidden coord-pair"><span><?= __("Gridsquare") ?>:&nbsp;</span><span class="text-success fw-bold" id="locator"></span></div>
-                <div class="cohidden coord-pair"><span><?= __("Distance") ?>:&nbsp;</span><span class="text-success fw-bold" id="distance"></span></div>
-                <div class="cohidden coord-pair"><span><?= __("Bearing") ?>:&nbsp;</span><span class="text-success fw-bold" id="bearing"></span></div>
-            </div>
-    </div>
+				<div class="cohidden coord-pair"><span><?= __("Latitude") ?>:&nbsp;</span><span class="text-success fw-bold" id="latDeg"></span></div>
+				<div class="cohidden coord-pair"><span><?= __("Longitude") ?>:&nbsp;</span><span class="text-success fw-bold" id="lngDeg"></span></div>
+				<div class="cohidden coord-pair"><span><?= __("Gridsquare") ?>:&nbsp;</span><span class="text-success fw-bold" id="locator"></span></div>
+				<div class="cohidden coord-pair"><span><?= __("Distance") ?>:&nbsp;</span><span class="text-success fw-bold" id="distance"></span></div>
+				<div class="cohidden coord-pair"><span><?= __("Bearing") ?>:&nbsp;</span><span class="text-success fw-bold" id="bearing"></span></div>
+			</div>
+		</div>
+	</div>
 </div>
 <script>
 var gridsquaremap = true;
@@ -122,17 +120,17 @@ var visitor = true;
 var visitor = false;
 <?php } ?>
 <?php
-    echo 'var jslayer ="' . $layer .'";';
-    echo "var jsattribution ='" . $attribution . "';";
-    echo "var homegrid ='" . strtoupper($homegrid[0]) . "';";
+	echo 'var jslayer ="' . $layer .'";';
+	echo "var jsattribution ='" . $attribution . "';";
+	echo "var homegrid ='" . strtoupper($homegrid[0]) . "';";
 
-    echo 'var gridsquares_gridsquares = "' . $gridsquares_gridsquares . '";';
-    echo 'var gridsquares_gridsquares_confirmed = "' . $gridsquares_gridsquares_confirmed . '";';
-    echo 'var gridsquares_gridsquares_not_confirmed = "' . $gridsquares_gridsquares_not_confirmed . '";';
-    echo 'var gridsquares_gridsquares_total_worked = "' . $gridsquares_gridsquares_total_activated . '";';
+	echo 'var gridsquares_gridsquares = "' . $gridsquares_gridsquares . '";';
+	echo 'var gridsquares_gridsquares_confirmed = "' . $gridsquares_gridsquares_confirmed . '";';
+	echo 'var gridsquares_gridsquares_not_confirmed = "' . $gridsquares_gridsquares_not_confirmed . '";';
+	echo 'var gridsquares_gridsquares_total_worked = "' . $gridsquares_gridsquares_total_activated . '";';
 	echo "var gridsquares_fields = \"" . $gridsquares_fields . "\";\n";
-    echo "var gridsquares_fields_confirmed = \"" . $gridsquares_fields_confirmed . "\";\n";
-    echo "var gridsquares_fields_not_confirmed = \"" . $gridsquares_fields_not_confirmed . "\";\n";
-    echo "var gridsquares_fields_total_worked = \"" . $gridsquares_fields_total_worked . "\";\n";
+	echo "var gridsquares_fields_confirmed = \"" . $gridsquares_fields_confirmed . "\";\n";
+	echo "var gridsquares_fields_not_confirmed = \"" . $gridsquares_fields_not_confirmed . "\";\n";
+	echo "var gridsquares_fields_total_worked = \"" . $gridsquares_fields_total_worked . "\";\n";
 ?>
 </script>
