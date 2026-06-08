6eda7aece996f101728aaa965d4fb478ca30f472
HB9HIL <fabian.berg@hb9hil.org>
2026-06-08 09:49:35 +0200

    proper auth check in oqrs

--- application/controllers/Oqrs.php
+++ application/controllers/Oqrs.php
@@ -9,9 +9,6 @@ class Oqrs extends CI_Controller {
 
 	function __construct() {
 		parent::__construct();
-		// Commented out to get public access
-		// $this->load->model('user_model');
-		// if(!$this->user_model->authorize(2)) { $this->session->set_flashdata('error', __("You're not allowed to do that!")); redirect('dashboard'); }
 		if (($this->config->item('disable_oqrs') ?? false)) { $this->session->set_flashdata('error', __("You're not allowed to do that!")); redirect('dashboard'); }
 	}
 
@@ -210,20 +207,21 @@ class Oqrs extends CI_Controller {
 	}
 
 	public function delete_oqrs_line() {
+		$this->_check_auth();
 		$id = $this->input->post('id', TRUE);
 		$this->load->model('oqrs_model');
 		$this->oqrs_model->delete_oqrs_line($id);
 	}
 
 	public function reject_oqrs_line() {
+		$this->_check_auth();
 		$id = $this->input->post('id', TRUE);
 		$this->load->model('oqrs_model');
 		$this->oqrs_model->reject_oqrs_line($id);
 	}
 
 	public function search_log() {
-		$this->load->model('user_model');
-		if(!$this->user_model->authorize(2) || !clubaccess_check(9)) { $this->session->set_flashdata('error', __("You're not allowed to do that!")); redirect('dashboard'); }
+		$this->_check_auth();
 
 		$this->load->model('oqrs_model');
 		$callsign = $this->input->post('callsign', TRUE);
@@ -236,8 +234,7 @@ class Oqrs extends CI_Controller {
 	}
 
 	public function search_log_time_date() {
-		$this->load->model('user_model');
-		if(!$this->user_model->authorize(2) || !clubaccess_check(9)) { $this->session->set_flashdata('error', __("You're not allowed to do that!")); redirect('dashboard'); }
+		$this->_check_auth();
 
 		// Get user-preferred date format
 		if ($this->session->userdata('user_date_format')) {
@@ -323,6 +320,7 @@ class Oqrs extends CI_Controller {
 	}
 
 	public function add_oqrs_to_print_queue() {
+		$this->_check_auth();
 		$this->load->model('oqrs_model');
 		$id = $this->input->post('id', TRUE);
 
@@ -330,6 +328,7 @@ class Oqrs extends CI_Controller {
 	}
 
 	public function mark_oqrs_line_as_done() {
+		$this->_check_auth();
 		$this->load->model('oqrs_model');
 		$id = $this->input->post('id', TRUE);
 
@@ -337,6 +336,7 @@ class Oqrs extends CI_Controller {
 	}
 
 	public function search() {
+		$this->_check_auth();
 		// Get Date format
 		if($this->session->userdata('user_date_format')) {
 			// If Logged in and session exists
@@ -368,10 +368,12 @@ class Oqrs extends CI_Controller {
 	}
 
 	public function status_info() {
+		$this->_check_auth();
 		$this->load->view('oqrs/status_info');
 	}
 
 	public function delete_oqrs_qso_match() {
+		$this->_check_auth();
 		$this->load->model('oqrs_model');
 		$id = $this->input->post('id', TRUE);
 		$qsoid = $this->input->post('qsoid', TRUE);
@@ -381,12 +383,22 @@ class Oqrs extends CI_Controller {
 	}
 
 	public function add_qso_match_to_oqrs() {
+		$this->_check_auth();
 		$this->load->model('oqrs_model');
 		$qsoid = $this->input->post('qsoid', TRUE);
 		$oqrsid = $this->input->post('oqrsid', TRUE);
 		$this->oqrs_model->add_qso_match_to_oqrs($qsoid, $oqrsid);
 		header('Content-Type: application/json');
 		echo json_encode(array('status' => 'success', 'message' => __("QSO match added successfully.")));
+	}
+
+	private function _check_auth() {
+		$this->load->model('user_model');
+		if(!$this->user_model->authorize(2) || !clubaccess_check(9)) { 
+			$this->session->set_flashdata('error', __("You're not allowed to do that!")); 
+			redirect('dashboard');
+			die;
+		}
 	}
 
 }
