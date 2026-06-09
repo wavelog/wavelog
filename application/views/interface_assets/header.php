67ee0f730fefdd34400aef555fd6f5665cf6471c
HB9HIL <fabian.berg@hb9hil.org>
2026-06-09 11:47:46 +0200

    keep the menu item but redirect to contesting

--- application/views/interface_assets/header.php
+++ application/views/interface_assets/header.php
@@ -495,6 +495,8 @@
 										<li><a class="dropdown-item" href="<?php echo site_url('csv'); ?>" title="SOTA CSV Export"><i class="fas fa-sync"></i> <?= __("SOTA CSV Export"); ?></a></li>
 
 										<li><a class="dropdown-item" href="<?php echo site_url('contesting'); ?>" title="Cabrillo Export"><i class="fas fa-sync"></i> <?= __("Cabrillo Export"); ?></a></li>
+
+										<li><a class="dropdown-item" href="<?php echo site_url('contesting'); ?>" title="EDI Export"><i class="fas fa-sync"></i> <?= __("EDI Export"); ?></a></li>
 									</ul>
 								</li>
 
