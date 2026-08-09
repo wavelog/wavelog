fcaaf5a6f97a3059df0033f44ec8c8d73bf84b88
Christoph Kottke, DG0TM <dg0tm@darc.de>
2026-08-09 21:55:28 +0200

    [Dashboard] Remove eQSL tooltip

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -497,7 +497,7 @@ function echo_table_header_col($name) {
 					<tr>
 						<th scope="row" width="50%"><?= __("eQSL"); ?></th>
 						<td width="50%">
-							<span title="<?= __("eQSL"); ?>" aria-label="<?= __("eQSL"); ?>: <?php echo $total_countries_confirmed_eqsl; ?>" data-bs-toggle="tooltip"><?php echo $total_countries_confirmed_eqsl; ?></span>
+							<span aria-label="<?= __("eQSL"); ?>: <?php echo $total_countries_confirmed_eqsl; ?>"><?php echo $total_countries_confirmed_eqsl; ?></span>
 						</td>
 					</tr>
 					<tr>
