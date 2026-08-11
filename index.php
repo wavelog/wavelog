a0350dbd05dec544d744fbfbda39042c970e3e62
HB9HIL <fabian.berg@hb9hil.org>
2026-08-11 11:35:34 +0200

    removed deprecated stuff

--- index.php
+++ index.php
@@ -86,11 +86,7 @@ switch (ENVIRONMENT)
 
 	case 'docker':
 		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>=')) {
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		} else {
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
+		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
 		if (($_ENV['DOCKER_DEVELOPMENT'] ?? false) == true) {
 			error_reporting(-1);
 			ini_set('display_errors', 1);
@@ -99,11 +95,7 @@ switch (ENVIRONMENT)
 
 	case 'production':
 		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>=')) {
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		} else {
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
+		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
 		break;
 
 	default:
