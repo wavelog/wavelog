644802a9838563dba4f1dbb00a98b3469ffff38f
phl0 <github@florian-wolters.de>
2026-08-11 13:39:27 +0200

    Handle case of no uploads (yet)

--- application/views/widgets/lotw_upload.php
+++ application/views/widgets/lotw_upload.php
@@ -99,8 +99,12 @@ This is a widget to show the last LoTW upload in your QRZ.com Bio or somewhere e
                     </tr>
                     <?php foreach ($lotw_uploads as $upload) { ?>
                         <tr>
-                        <td><span class="lotw-upload-label"><?php echo $upload->callsign.":"; ?></span></td>
-                        <td><span class="lotw-upload-label"><?php echo $upload->last_upload; ?>Z</span></td>
+                            <td><span class="lotw-upload-label"><?php echo $upload->callsign.":"; ?></span></td>
+                            <?php if ($upload->last_upload) { ?>
+                                <td><span class="lotw-upload-label"><?php echo $upload->last_upload; ?>Z</span></td>
+                            <?php } else { ?>
+                                <td><span class="lotw-upload-label"><?= __("n/a"); ?></span></td>
+                            <?php } ?>
                         </tr>
                     <?php } ?>
                     </table>
