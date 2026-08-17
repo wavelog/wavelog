743c72d21b0251b849e0faaef3b58970993e2536
HB9HIL <fabian.berg@hb9hil.org>
2026-08-17 20:57:45 +0200

    fix alaska and add DC

--- application/models/Counties.php
+++ application/models/Counties.php
@@ -167,6 +167,7 @@ class Counties extends CI_Model
     private $us_state_codes = array(
         'Alabama' => 'AL', 'Alaska' => 'AK', 'Arizona' => 'AZ', 'Arkansas' => 'AR',
         'California' => 'CA', 'Colorado' => 'CO', 'Connecticut' => 'CT', 'Delaware' => 'DE',
+        'District of Columbia' => 'DC',
         'Florida' => 'FL', 'Georgia' => 'GA', 'Hawaii' => 'HI', 'Idaho' => 'ID',
         'Illinois' => 'IL', 'Indiana' => 'IN', 'Iowa' => 'IA', 'Kansas' => 'KS',
         'Kentucky' => 'KY', 'Louisiana' => 'LA', 'Maine' => 'ME', 'Maryland' => 'MD',
