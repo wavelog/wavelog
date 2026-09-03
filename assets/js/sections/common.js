a7c6eba534240dc24a527f2852d703a51eb5c64d
phl0 <github@florian-wolters.de>
2026-09-03 09:54:41 +0200

    Add gridsquare toggle to QSO map

--- assets/js/sections/common.js
+++ assets/js/sections/common.js
@@ -224,7 +224,7 @@ function displayQso(id) {
                         maxZoom: 18,
                         attribution: option_map_tile_server_copyright,
                     }).addTo(mymap);
-
+                    var layerControl = new L.Control.Layers(null, { [lang_general_gridsquares]: maidenhead = L.maidenhead() }).addTo(mymap);
 
                     var printer = L.easyPrint({
                         tileLayer: tiles,
